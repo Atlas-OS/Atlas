7200f6a482300f283b06dd8720f6637b7b2dafa9
Zusier <zusier@disroot.org>
2021-11-16 21:29:52 -0600

    More organizing

--- src/20H2/AtlasModules/atlas-config.bat
+++ src/20H2/AtlasModules/atlas-config.bat
@@ -2038,6 +2038,8 @@ cmd /c scoop install git -g
 call C:\Windows\AtlasModules\refreshenv.bat
 echo Adding extras bucket...
 cmd /c scoop bucket add extras
+goto finish
+
 :browser
 multiplechoice "Ungoogled-Chromium;Firefox;Brave;GoogleChrome;" "Pick a Browser" "Browser" > C:\Windows\AtlasModules\tmp.txt
 for /f %%i in (C:\Windows\AtlasModules\tmp.txt) do (
@@ -2045,10 +2047,12 @@ for /f %%i in (C:\Windows\AtlasModules\tmp.txt) do (
 	pause
 	set filtered=!filter:;= !
 )
-pause
 ::if "%filtered%" == "" echo You need to install a browser! You will need it later on. && pause && goto browser
 :: must launch in separate process, scoop seems to exit the whole script if not
 cmd /c scoop install %filtered% -g
+goto finish
+
+:altSoftware
 :: Findstr for 7zip-zstd, add versions bucket if errlvl 0
 set filter=""
 multiplechoice "discord;bleachbit;notepadplusplus;msiafterburner;rtss;steam;thunderbird;foobar2000;irfanview;git;mpv;vlc;vscode;putty;ditto;" "Install Common Software" "Common Software" > C:\Windows\AtlasModules\tmp.txt
