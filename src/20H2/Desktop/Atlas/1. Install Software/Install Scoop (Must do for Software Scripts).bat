1efcf7293fde99d062a34cb8f9d7ef2285d9e7ba
Zusier <zusier@disroot.org>
2021-11-22 20:16:37 -0600

    Fix scoop installing under TrustedInstaller

--- src/20H2/Desktop/Atlas/1. Install Software/Install Scoop (Must do for Software Scripts).bat
+++ src/20H2/Desktop/Atlas/1. Install Software/Install Scoop (Must do for Software Scripts).bat
@@ -1,2 +1,2 @@
 @echo off
-C:\Windows\AtlasModules\nsudo -U:T -P:E -UseCurrentConsole -Wait C:\Windows\AtlasModules\atlas-config.bat /scoop
+C:\Windows\AtlasModules\nsudo -U:C -P:E -UseCurrentConsole -Wait C:\Windows\AtlasModules\atlas-config.bat /scoop
