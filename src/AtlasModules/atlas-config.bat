8edbf0f7d9680fc3d6907fe717c123a1799a67b6
Xyueta <vojtech@disroot.org>
2022-12-13 20:46:15 +0100

    fix formatting

--- src/AtlasModules/atlas-config.bat
+++ src/AtlasModules/atlas-config.bat
@@ -61,7 +61,7 @@ if /i "%~1"=="/ei"         goto indexE
 if /i "%~1"=="/dw"         goto wifiD
 if /i "%~1"=="/ew"         goto wifiE
 
-:: Hyper-V
+:: Hyper-V and VBS
 if /i "%~1"=="/dhyper"         goto hyperD
 if /i "%~1"=="/ehyper"         goto hyperE
 
