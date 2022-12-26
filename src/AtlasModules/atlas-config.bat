90a8f8df2fa1caa279b06e366f63f95f35594138
Xyueta <vojtech@disroot.org>
2022-12-26 18:44:07 +0100

    edit info

--- src/AtlasModules/atlas-config.bat
+++ src/AtlasModules/atlas-config.bat
@@ -1309,7 +1309,7 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsRunInBa
 %currentuser% reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /v "GlobalUserDisabled" /t REG_DWORD /d "1" /f
 %currentuser% reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "BackgroundAppGlobalToggle" /t REG_DWORD /d "0" /f
 
-:: set Win32PrioritySeparation to short variable 1:1
+:: set Win32PrioritySeparation to short variable 1:1, no foreground boost
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "36" /f
 
 :: disable notifications/action center
