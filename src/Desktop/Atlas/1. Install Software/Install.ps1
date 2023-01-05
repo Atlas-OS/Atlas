1db738fd3748f9e57ee3b4bb0a35dc373219fd52
Jay <54886938+JayXTQ@users.noreply.github.com>
2023-01-05 18:42:39 +0000

    Compiled all install scripts into 1 (#466)
    There is also the choice of a manual method if people would prefer it that way

---
+++ src/Desktop/Atlas/1. Install Software/Install.ps1
@@ -0,0 +1,53 @@
+function WriteList ($list, $name) {
+    $listname = $list
+
+    Write-Host "Please select a $name to install:"
+    For ($i=0; $i -lt $listname.Count; $i++)  {
+    Write-Host "$($i+1): $($listname[$i])"
+    }
+
+    [int]$number = Read-Host "Enter the number to select the option"
+
+    Write-Host "You've selected $($listname[$number-1])."
+    return $listname[$number-1]
+}
+
+$provider = WriteList -list @("Chocolatey", "Scoop") -name "provider"
+
+function InstallBrowser {
+    $script = $PSScriptRoot+"\Manual\Install Browser via $provider.ps1"
+    & $script
+}
+
+function InstallAltSoft {
+    $script = $PSScriptRoot+"\Manual\Install Other Software via $provider.bat"
+    & $script
+}
+
+function InstallSoftware {
+    $type = WriteList -list @("Browser", "Other Software") -name "type of software"
+    if ($type -eq "Browser") {
+        InstallBrowser
+    } else {
+        InstallAltSoft
+    }
+}
+
+if ($provider -eq "Chocolatey") {
+    $ChocoInstalled = $false
+    if (Get-Command choco -ErrorAction SilentlyContinue) {
+        $ChocoInstalled = $true
+    } else {
+        Write-Host "Chocolatey not found, installing..."
+        C:\Windows\AtlasModules\NSudo.exe -U:C -P:E -UseCurrentConsole -Wait C:\Windows\AtlasModules\atlas-config.bat /choco
+    }
+} else {
+    $ScoopInstalled = $false
+    if (Get-Command scoop -ErrorAction SilentlyContinue) {
+        $ScoopInstalled = $true
+    } else {
+        Write-Host "Scoop not found, installing..."
+        C:\Windows\AtlasModules\NSudo.exe -U:C -P:E -UseCurrentConsole -Wait C:\Windows\AtlasModules\atlas-config.bat /scoop
+    }
+}
+InstallSoftware
