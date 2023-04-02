d65b1e03c9fc3efce44591073e8fccc36c0e9d31
Xyueta <vojtech@disroot.org>
2023-04-02 12:02:22 +0200

    fix: :bug: bug fixes from version 0.1

---
+++ src/Executables/Atlas/3. Configuration/1. General Configuration/.NET 3.5/Enable .NET 3.5.cmd
@@ -0,0 +1,13 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
+	call RunAsTI.cmd "%~f0" "%*"
+	exit /b 0
+)
+
+DISM /Online /Enable-Feature /FeatureName:NetFx3 /All /NoRestart
+
+echo Finished, please reboot your device for changes to apply.
+pause
+exit /b
