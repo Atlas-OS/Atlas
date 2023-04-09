375e14328384840ef77b30c5b191c222c8b7c201
Xyueta <vojtech@disroot.org>
2023-04-09 17:31:45 +0200

    fix: :bug: re-add ``Set the active scheme as the current scheme``

--- src/Executables/POWER.cmd
+++ src/Executables/POWER.cmd
@@ -62,6 +62,9 @@ if "!DEVICE_TYPE!" == "PC" (
     rem Turn off display after - 0 seconds
     powercfg -setacvalueindex scheme_current 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
 
+    rem Set the active scheme as the current scheme
+    powercfg -setactive scheme_current
+
     rem Unhide power scheme attributes
     rem Source: https://gist.github.com/Velocet/7ded4cd2f7e8c5fa475b8043b76561b5#file-unlock-powercfg-ps1
     PowerShell -NoP -C "$PowerCfg = (Get-ChildItem 'HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings' -Recurse).Name -notmatch '\bDefaultPowerSchemeValues|(\\[0-9]|\b255)$';foreach ($item in $PowerCfg) { Set-ItemProperty -Path $item.Replace('HKEY_LOCAL_MACHINE','HKLM:') -Name 'Attributes' -Value 0 -Force}"
