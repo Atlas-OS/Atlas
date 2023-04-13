68973aa095ad9755b2ea77985c25cffeb7b17557
Xyueta <vojtech@disroot.org>
2023-04-13 15:20:49 +0200

    fix: :sparkles: network discovery/sharing and add disable script

---
+++ src/Executables/Atlas/3. Configuration/1. General Configuration/Network Discovery/Disable Network Discovery (default).cmd
@@ -0,0 +1,27 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
+	call RunAsTI.cmd "%~f0" "%*"
+	exit /b
+)
+
+:: Disable Lanman Workstation (SMB)
+DISM /Online /Disable-Feature /FeatureName:SmbDirect /NoRestart
+call setSvc.cmd KSecPkg 4
+call setSvc.cmd LanmanWorkstation 4
+call setSvc.cmd mrxsmb 4
+call setSvc.cmd mrxsmb20 4
+call setSvc.cmd rdbss 3
+call setSvc.cmd srv2 4
+
+call setSvc.cmd NlaSvc 2
+call setSvc.cmd lmhosts 4
+call setSvc.cmd netman 3
+call setSvc.cmd SSDPSRV 4
+call setSvc.cmd fdPHost 4
+call setSvc.cmd FDResPub 4
+
+echo Finished, please reboot your device for changes to apply.
+pause
+exit /b
--- src/Executables/Atlas/3. Configuration/1. General Configuration/Network Discovery/Enable Network Discovery.cmd
+++ src/Executables/Atlas/3. Configuration/1. General Configuration/Network Discovery/Enable Network Discovery.cmd
@@ -6,10 +6,25 @@ whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 	exit /b
 )
 
+:: Enable Lanman Workstation (SMB) as a dependency
+call setSvc.cmd KSecPkg 0
+call setSvc.cmd LanmanWorkstation 2
+call setSvc.cmd mrxsmb 3
+call setSvc.cmd mrxsmb20 3
+call setSvc.cmd rdbss 1
+call setSvc.cmd srv2 3
+
+DISM /Online /Enable-Feature /FeatureName:SmbDirect /NoRestart
+
+:: Enable EventLog as a dependency
 call setSvc.cmd eventlog 2
+
 call setSvc.cmd NlaSvc 2
 call setSvc.cmd lmhosts 3
 call setSvc.cmd netman 3
+call setSvc.cmd SSDPSRV 3
+call setSvc.cmd fdPHost 3
+call setSvc.cmd FDResPub 3
 
 echo Finished, please reboot your device for changes to apply.
 pause
