0a9ccdb0579d4f8bce7eedb859885ee56f9ac0f1
Xyueta <vojtech@disroot.org>
2023-04-15 21:34:09 +0200

    feat: :zap: error checking for Static IP script {2)

--- src/Executables/Atlas/3. Configuration/2. Advanced Configuration/Static IP/Automatically Set Static IP.cmd
+++ src/Executables/Atlas/3. Configuration/2. Advanced Configuration/Static IP/Automatically Set Static IP.cmd
@@ -10,7 +10,7 @@ ping -n 1 -4 1.1.1.1 | find "time=" > nul 2>&1
 if !errorlevel! == 1 (
 	echo You must have an internet connection to use this script.
 	pause
-	exit /b
+	exit /b 1
 )
 
 set /P DNS1="Set primary DNS Server (e.g. 1.1.1.1): "
@@ -20,12 +20,27 @@ for /f "tokens=3" %%a in ('netsh int ip show config name^="!DeviceName!" ^| find
 for /f "tokens=3" %%a in ('netsh int ip show config name^="!DeviceName!" ^| findstr "Default Gateway:"') do set DHCPGateway=%%a
 for /f "tokens=2 delims=()" %%a in ('netsh int ip show config name^="!DeviceName!" ^| findstr /r "(.*)"') do for %%i in (%%a) do set DHCPSubnetMask=%%i
 
-:: set static ip
+:: Check for errors and exit if invalid
+cls
+if "!DeviceName!" == "" set incorrectIP=1
+call:isValidIP !LocalIP!
+call:isValidIP !DHCPGateway!
+call:isValidIP !DHCPSubnetMask!
+call:isValidIP !DNS1!
+call:isValidIP !DNS2!
+
+if !incorrectIP! == 1 (
+	echo Setting a Static IP address failed.
+	pause
+	exit /b 1
+)
+
+:: Set Static IP
 netsh int ipv4 set address name="!DeviceName!" static !LocalIP! !DHCPSubnetMask! !DHCPGateway! > nul 2>&1
 netsh int ipv4 set dns name="!DeviceName!" static !DNS1! primary > nul 2>&1
 netsh int ipv4 add dns name="!DeviceName!" !DNS2! index=2 > nul 2>&1
 
-:: display details about the connection
+:: Display details about the connection
 echo Interface: !DeviceName!
 echo Private IP: !LocalIP!
 echo Subnet Mask: !DHCPSubnetMask%!
@@ -35,7 +50,8 @@ echo Alternate DNS: !DNS2!
 echo.
 echo If this information appears to be incorrect or is blank, please report it on Discord or Github.
 
-choice /c:yn /n /m "Do you want to disable static ip services (break internet icon)? [Y/N] "
+echo]
+choice /c:yn /n /m "Do you want to disable Static IP services (break internet icon)? [Y/N] "
 if !errorlevel! == 1 goto staticIPS
 if !errorlevel! == 2 goto staticIPC
 goto staticIPS
@@ -49,3 +65,16 @@ call setSvc.cmd NlaSvc 4
 echo Finished, please reboot your device for changes to apply.
 pause
 exit /b
+
+:isValidIP
+:: Credit to Phlegm for error checking
+for /f "tokens=1-4 delims=./" %%a in ("%1") do (
+	if %%a LSS 1 set incorrectIP=1
+	if %%a GTR 255 set incorrectIP=1
+	if %%b LSS 0 set incorrectIP=1
+	if %%b GTR 255 set incorrecIP=1
+	if %%c LSS 0 set incorrectIP=1
+	if %%c GTR 255 set incorrectIP=1
+	if %%d LSS 0 set incorrectIP=1
+	if %%d GTR 255 set incorrectIP=1
+)
