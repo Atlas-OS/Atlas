4670389c06dacf3bf50c5aac2a91b9f235124fe3
Xyueta <vojtech@disroot.org>
2023-04-15 11:55:01 +0200

    feat: :sparkles: new software installation GUI

--- src/Executables/Atlas/1. Software/Install Software.ps1
+++ src/Executables/Atlas/1. Software/Install Software.ps1
@@ -1,57 +1,243 @@
-function WriteList ($list, $name) {
-    $listname = $list
-    Start-Sleep -Seconds 2
-    Clear-Host
+# Credit to spddl for part of the code
+# Require admin privileges if User Account Control is enabled
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+    Start-Process PowerShell "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
 
-    Write-Host "Please select a $name to install:"
-    For ($i=0; $i -lt $listname.Count; $i++)  {
-    Write-Host "$($i+1): $($listname[$i])"
+$ErrorActionPreference = 'SilentlyContinue'
+
+[int] $global:column = 0
+[int] $maxColumn = 1
+[int] $separate = 30
+[int] $global:lastPos = 50
+[bool]$global:install = $false
+
+function generate_checkbox {
+    param(
+        [string]$checkboxText,
+        [string]$package,
+        [bool]$enabled = $true
+    )
+    $checkbox = new-object System.Windows.Forms.checkbox
+    if ($global:column -ge $maxColumn) {
+        $checkbox.Location = new-object System.Drawing.Size(($global:column * 300), $global:lastPos)
+        $global:column = 0
+        $global:lastPos += $separate
     }
-
-    [int]$number = Read-Host "Enter the number to select the option"
-    Write-Host "You have selected $($listname[$number-1])."
-    Start-Sleep -Seconds 2
-    Clear-Host
-
-    return $listname[$number-1]
-}
-
-$provider = WriteList -list @("Chocolatey", "Scoop") -name "provider"
-
-function InstallBrowser {
-    $script = $PSScriptRoot+"\Manual\Install Browser via $provider.ps1"
-    & $script
-}
-
-function InstallAltSoft {
-    $script = $PSScriptRoot+"\Manual\Install Other Software via $provider.ps1"
-    & $script
-}
-
-function InstallSoftware {
-    $type = WriteList -list @("Browser", "Other Software") -name "type of software"
-    if ($type -eq "Browser") {
-        InstallBrowser
-    } else {
-        InstallAltSoft
+    else {
+        $checkbox.Location = new-object System.Drawing.Size(30, $global:lastPos)
+        $global:column = $column + 1
     }
+    $checkbox.Size = new-object System.Drawing.Size(250, 18)
+    $checkbox.Text = $checkboxText
+    $checkbox.Name = $package
+    $checkbox.Enabled = $enabled
+    
+    $checkbox
 }
 
-if ($provider -eq "Chocolatey") {
-    $ChocoInstalled = $false
-    if (Get-Command choco -ErrorAction SilentlyContinue) {
-        $ChocoInstalled = $true
-    } else {
-        Write-Host "Chocolatey not found, installing..."
-        & "$PSScriptRoot\Manual\Install $provider.ps1"
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+
+# Set the size of your form
+$Form = New-Object System.Windows.Forms.Form
+$Form.Text = "Install Software | Atlas" # Titlebar
+$Form.ShowIcon = $false
+$Form.MaximizeBox = $false
+$Form.MinimizeBox = $false
+$Form.Size = New-Object System.Drawing.Size(600, 210)
+$Form.AutoSizeMode = 0
+$Form.KeyPreview = $True
+$Form.SizeGripStyle = 2
+
+# Label
+$Label = New-Object System.Windows.Forms.label
+$Label.Location = New-Object System.Drawing.Size(11, 15)
+$Label.Size = New-Object System.Drawing.Size(255, 15)
+$Label.Text = "Download and install software using Chocolatey:"
+$Form.Controls.Add($Label)
+
+# https://community.chocolatey.org/packages/ungoogled-chromium
+$Form.Controls.Add((generate_checkbox "ungoogled-chromium" "ungoogled-chromium"))
+
+# https://community.chocolatey.org/packages/Firefox
+$Form.Controls.Add((generate_checkbox "Mozilla Firefox" "firefox"))
+
+# https://community.chocolatey.org/packages/brave
+$Form.Controls.Add((generate_checkbox "Brave Browser" "brave"))
+
+# https://community.chocolatey.org/packages/GoogleChrome
+$Form.Controls.Add((generate_checkbox "Google Chrome" "googlechrome"))
+
+# https://community.chocolatey.org/packages/librewolf
+$Form.Controls.Add((generate_checkbox "LibreWolf" "librewolf"))
+
+# https://community.chocolatey.org/packages/tor-browser
+$Form.Controls.Add((generate_checkbox "Tor Browser" "tor-browser"))
+
+# https://community.chocolatey.org/packages/discord
+$Form.Controls.Add((generate_checkbox "Discord" "discord"))
+
+# https://community.chocolatey.org/packages/discord-canary
+$Form.Controls.Add((generate_checkbox "Discord Canary" "discord-canary"))
+
+# https://community.chocolatey.org/packages/steam
+$Form.Controls.Add((generate_checkbox "Steam" "steam"))
+
+# https://community.chocolatey.org/packages/playnite
+$Form.Controls.Add((generate_checkbox "Playnite" "playnite"))
+
+# https://community.chocolatey.org/packages/legendary
+$Form.Controls.Add((generate_checkbox "legendary" "legendary"))
+
+# https://community.chocolatey.org/packages/Everything
+$Form.Controls.Add((generate_checkbox "Everything" "everything"))
+
+# https://community.chocolatey.org/packages/msiafterburner
+$Form.Controls.Add((generate_checkbox "MSI Afterburner" "msiafterburner"))
+
+# https://community.chocolatey.org/packages/thunderbird
+$Form.Controls.Add((generate_checkbox "Mozilla Thunderbird" "thunderbird"))
+
+# https://community.chocolatey.org/packages/foobar2000
+$Form.Controls.Add((generate_checkbox "foobar2000" "foobar2000"))
+
+# https://community.chocolatey.org/packages/irfanview
+$Form.Controls.Add((generate_checkbox "IrfanView" "irfanview"))
+
+# https://community.chocolatey.org/packages/git
+$Form.Controls.Add((generate_checkbox "Git" "git"))
+
+# https://community.chocolatey.org/packages/mpv
+$Form.Controls.Add((generate_checkbox "mpv" "mpv"))
+
+# https://community.chocolatey.org/packages/vlc
+$Form.Controls.Add((generate_checkbox "VLC" "vlc"))
+
+# https://community.chocolatey.org/packages/putty
+$Form.Controls.Add((generate_checkbox "PuTTY" "putty"))
+
+# https://community.chocolatey.org/packages/ditto
+$Form.Controls.Add((generate_checkbox "Ditto" "ditto"))
+
+# https://community.chocolatey.org/packages/7zip
+$Form.Controls.Add((generate_checkbox "7-Zip" "7zip"))
+
+# https://community.chocolatey.org/packages/teamspeak
+$Form.Controls.Add((generate_checkbox "Teamspeak" "teamspeak"))
+
+# https://community.chocolatey.org/packages/spotify
+$Form.Controls.Add((generate_checkbox "Spotify" "spotify"))
+
+# https://community.chocolatey.org/packages/obs-studio
+$Form.Controls.Add((generate_checkbox "OBS Studio" "obs-studio"))
+
+# https://community.chocolatey.org/packages/msiafterburner
+$Form.Controls.Add((generate_checkbox "MSI Afterburner" "msiafterburner"))
+
+# https://community.chocolatey.org/packages/cpu-z
+$Form.Controls.Add((generate_checkbox "CPU-Z" "cpu-z"))
+
+# https://community.chocolatey.org/packages/gpu-z
+$Form.Controls.Add((generate_checkbox "GPU-Z" "gpu-z"))
+
+# https://community.chocolatey.org/packages/notepadplusplus
+$Form.Controls.Add((generate_checkbox "Notepad++" "notepadplusplus"))
+
+# https://community.chocolatey.org/packages/vscode
+$Form.Controls.Add((generate_checkbox "VSCode" "vscode"))
+
+# https://community.chocolatey.org/packages/bulk-crap-uninstaller
+$Form.Controls.Add((generate_checkbox "Bulk Crap Uninstaller" "bulk-crap-uninstaller"))
+
+# https://community.chocolatey.org/packages/hwinfo
+$Form.Controls.Add((generate_checkbox "HWiNFO" "hwinfo"))
+
+if ($global:column -ne 0) {
+    $global:lastPos += $separate
+}
+
+$Form.height = $global:lastPos + 80
+
+# Dark Mode/Light Mode Toggle
+$ToggleBtn = New-Object System.Windows.Forms.Button
+$ToggleBtn.Location = New-Object System.Drawing.Point(500, 20)
+$ToggleBtn.Size = New-Object System.Drawing.Size(80, 23)
+$ToggleBtn.Text = "Dark Mode"
+$ToggleBtn.Add_Click({
+if ($this.Text -eq "Dark Mode") {
+    $this.Text = "Light Mode"
+    $Form.BackColor = [System.Drawing.Color]::FromArgb(64, 64, 64)
+    $Form.ForeColor = [System.Drawing.Color]::White
+    foreach ($control in $Form.Controls) {
+        if ($control.GetType().Name -eq "Checkbox") {
+            $control.BackColor = [System.Drawing.Color]::FromArgb(64, 64, 64)
+            $control.ForeColor = [System.Drawing.Color]::White
+        }
     }
 } else {
-    $ScoopInstalled = $false
-    if (Get-Command scoop -ErrorAction SilentlyContinue) {
-        $ScoopInstalled = $true
-    } else {
-        Write-Host "Scoop not found, installing..."
-        & "$PSScriptRoot\Manual\Install $provider.ps1"
+    $this.Text = "Dark Mode"
+    $Form.BackColor = [System.Drawing.Color]::White
+    $Form.ForeColor = [System.Drawing.Color]::Black
+    foreach ($control in $Form.Controls) {
+        if ($control.GetType().Name -eq "Checkbox") {
+            $control.BackColor = [System.Drawing.Color]::White
+            $control.ForeColor = [System.Drawing.Color]::Black
+        }
     }
 }
-InstallSoftware
+})
+$Form.Controls.Add($ToggleBtn)
+
+# Install Button
+$lastPosWidth = $form.Width - 80 - 31
+$InstallButton = new-object System.Windows.Forms.Button
+$InstallButton.Location = new-object System.Drawing.Size($lastPosWidth, $global:lastPos)
+$InstallButton.Size = new-object System.Drawing.Size(80, 23)
+$InstallButton.Text = "Install"
+$InstallButton.Add_Click({
+    $checkedBoxes = $Form.Controls | Where-Object { $_ -is [System.Windows.Forms.Checkbox] -and $_.Checked }
+    if ($checkedBoxes.Count -eq 0) {
+        [System.Windows.Forms.MessageBox]::Show("Please select at least one software package to install.", "No package selected", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    }
+    else {
+        $global:install = $true
+        $Form.Close()
+    }
+})
+$Form.Controls.Add($InstallButton)
+
+# Activate the form
+$Form.Add_Shown({ $Form.Activate() })
+[void] $Form.ShowDialog()
+
+if ($global:install) {
+    $installPackages = [System.Collections.ArrayList]::new()
+    $installSeparatedPackages = [System.Collections.ArrayList]::new()
+    $Form.Controls | Where-Object { $_ -is [System.Windows.Forms.Checkbox] } | ForEach-Object {
+        if ($_.Checked) {
+            if ($_.Name.contains("--")) {
+                [void]$installSeparatedPackages.Add($_.Name)
+            }
+            else {
+                [void]$installPackages.Add($_.Name)
+            }
+        }
+    }
+
+    if ($installPackages.count -ne 0) {
+        Write-Host "$Env:ProgramData\chocolatey\choco.exe install $($installPackages -join ' ') -y"
+        Start-Process -FilePath "$Env:ProgramData\chocolatey\choco.exe" -ArgumentList "install $($installPackages -join ' ') -y --ignore-checksums" -Wait
+    }
+    if ($installSeparatedPackages.count -ne 0) {
+        foreach ($paket in $installSeparatedPackages) {
+            Write-Host "$Env:ProgramData\chocolatey\choco.exe install $paket -y"
+            Start-Process -FilePath "$Env:ProgramData\chocolatey\choco.exe" -ArgumentList "install $paket -y --ignore-checksums" -Wait
+            if ($paket.contains("--version")) {
+                Write-Host "$Env:ProgramData\chocolatey\choco.exe pin add -n $($paket.split(' ')[0])"
+                Start-Process -FilePath "$Env:ProgramData\chocolatey\choco.exe" -ArgumentList "pin add -n $($paket.split(' ')[0])" -Wait
+            }
+        }
+    }
+}
