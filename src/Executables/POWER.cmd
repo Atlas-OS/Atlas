1f7c91eec61510bb69befeed21a7fb9fc943c775
Xyueta <vojtech@disroot.org>
2023-04-22 20:18:29 +0200

    feat: :art: clean up the config.yml and add back the power saving script

--- src/Executables/POWER.cmd
+++ src/Executables/POWER.cmd
@@ -72,6 +72,25 @@ if "!DEVICE_TYPE!" == "PC" (
     rem Disable driver power saving
     PowerShell -NoP -C "$usb_devices = @('Win32_USBController', 'Win32_USBControllerDevice', 'Win32_USBHub'); $power_device_enable = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi; foreach ($power_device in $power_device_enable){$instance_name = $power_device.InstanceName.ToUpper(); foreach ($device in $usb_devices){foreach ($hub in Get-WmiObject $device){$pnp_id = $hub.PNPDeviceID; if ($instance_name -like \"*$pnp_id*\"){$power_device.enable = $False; $power_device.psbase.put()}}}}"
 
+    for %%a in (
+        "AllowIdleIrpInD3"
+        "D3ColdSupported"
+        "DeviceSelectiveSuspended"
+        "EnableIdlePowerManagement"
+        "EnableSelectiveSuspend"
+        "EnhancedPowerManagementEnabled"
+        "IdleInWorkingState"
+        "SelectiveSuspendEnabled"
+        "SelectiveSuspendOn"
+        "WaitWakeEnabled"
+        "WakeEnabled"
+        "WdfDirectedPowerTransitionEnable"
+    ) do (
+        for /f "delims=" %%b in ('reg query "HKLM\SYSTEM\CurrentControlSet\Enum" /s /f "%%~a" ^| findstr "HKEY"') do (
+            reg add "%%b" /v "%%~a" /t REG_DWORD /d "0" /f
+        )
+    )
+
     rem Disable power throttling
     rem https://blogs.windows.com/windows-insider/2017/04/18/introducing-power-throttling
     reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" /v "PowerThrottlingOff" /t REG_DWORD /d "1" /f
