71bd7f6aaf1156ba89cfa0b6d51ff4e591ee9b6e
Xyueta <vojtech@disroot.org>
2023-04-23 12:35:20 +0200

    feat: :sparkles: fix services/drivers script and merge into one

--- src/Executables/BACKUP1.cmd
+++ src/Executables/BACKUP1.cmd
@@ -1,38 +1,19 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: Backup default windows services
-set "filename=C:!HOMEPATH!\Desktop\Atlas\4. Troubleshooting\Services\Default Windows services.reg"
-if exist "!filename!" goto :drivers
-
-echo Windows Registry Editor Version 5.00 >> "!filename!"
-echo] >> "!filename!"
-for /f "skip=1" %%a in ('wmic service get Name ^| findstr "[a-z]" ^| findstr /v "TermService"') do (
-	set svc=%%a
-	set svc=!svc: =!
-	for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\!svc!" /t REG_DWORD /s /c /f "Start" /e ^| findstr "[0-4]$"') do (
-		set /a start=%%a
-		echo !start!
-		echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> "!filename!"
-		echo "Start"=dword:0000000!start! >> "!filename!"
-		echo] >> "!filename!"
-	)
-)
-
-:drivers
-:: Backup default windows drivers
-set "filename=C:!HOMEPATH!\Desktop\Atlas\4. Troubleshooting\Services\Default Windows drivers.reg"
+:: Backup default Atlas Services and Drivers
+set filename="C:!HOMEPATH!\Desktop\Atlas\4. Troubleshooting\Services\Default Windows Services and Drivers.reg"
 if exist "!filename!" exit /b
 
-echo Windows Registry Editor Version 5.00 >> "!filename!"
-echo] >> "!filename!"
-for /f "delims=," %%a in ('driverquery /FO CSV') do (
-	set svc=%%a
-	for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\!svc!" /t REG_DWORD /s /c /f "Start" /e ^| findstr "[0-4]$"') do (
-		set /a start=%%a
-		echo !start!
-		echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> "!filename!"
-		echo "Start"=dword:0000000!start! >> "!filename!"
-		echo] >> "!filename!"
-	)
+echo Windows Registry Editor Version 5.00 >> !filename!
+for /f "delims=" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services"') do (
+	for /f "tokens=3" %%b in ('reg query "%%~a" /v "Start"') do (
+		for /l %%c in (0,1,4) do (
+			if "%%b"=="0x%%c" (
+				echo. >> !filename!
+				echo [%%~a] >> !filename!
+				echo "Start"=dword:0000000%%c >> !filename!
+			) 
+		) 
+	) 
 )
