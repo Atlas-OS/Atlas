8d12cf5337370a9d3f493b0023cbc5a5055a8975
Xyueta <vojtech@disroot.org>
2023-04-29 17:39:19 +0200

    feat: :sparkles: robocopy -> xcopy

--- src/Executables/COPYDESKTOP.cmd
+++ src/Executables/COPYDESKTOP.cmd
@@ -2,5 +2,5 @@
 setlocal EnableDelayedExpansion
 
 for /f "usebackq delims=" %%a in (`dir /b /a:d "!SystemDrive!\Users" ^| findstr /v /i /x /c:"Public" /c:"Default User" /c:"All Users"`) do (
-	robocopy "Atlas" "!SystemDrive!\Users\%%a\Desktop\Atlas " /e /purge /im /it /np
+	xcopy "Atlas" "!SystemDrive!\Users\%%a\Desktop\Atlas" /E /Y /I
 )
