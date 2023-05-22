a3aec59032fe7f020828db66415613a8e1b0125f
he3als <65787561+he3als@users.noreply.github.com>
2023-05-22 16:36:59 +0100

    chore: consistent whitespace in ps1 scripts

--- src/Executables/DISABLEPNP.ps1
+++ src/Executables/DISABLEPNP.ps1
@@ -1,34 +1,34 @@
 # Disable PnP (plug and play) devices
 $devices = @(
-    "AMD PSP",
-    "AMD SMBus",
-    "Base System Device",
-    "*Bluetooth*",
-    "Composite Bus Enumerator",
-    "Direct memory access controller"
-    "High precision event timer",
-    "Intel Management Engine",
-    "Intel SMBus",
-    "Legacy device"
-    "Microsoft Kernel Debug Network Adapter",
-    "Microsoft RRAS Root Enumerator",
-    "Motherboard resources",
-    "NDIS Virtual Network Adapter Enumerator",
-    "Numeric Data Processor",
-    "PCI Data Acquisition and Signal Processing Controller",
-    "PCI Encryption/Decryption Controller",
-    "PCI Memory Controller",
-    "PCI Simple Communications Controller",
-    "PCI standard RAM Controller",
+	"AMD PSP",
+	"AMD SMBus",
+	"Base System Device",
+	"*Bluetooth*",
+	"Composite Bus Enumerator",
+	"Direct memory access controller"
+	"High precision event timer",
+	"Intel Management Engine",
+	"Intel SMBus",
+	"Legacy device"
+	"Microsoft Kernel Debug Network Adapter",
+	"Microsoft RRAS Root Enumerator",
+	"Motherboard resources",
+	"NDIS Virtual Network Adapter Enumerator",
+	"Numeric Data Processor",
+	"PCI Data Acquisition and Signal Processing Controller",
+	"PCI Encryption/Decryption Controller",
+	"PCI Memory Controller",
+	"PCI Simple Communications Controller",
+	"PCI standard RAM Controller",
 	# Commented as Remote Desktop is meant to be supported
-    # "Remote Desktop Device Redirector Bus",
-    "SM Bus Controller",
-    "System CMOS/real time clock",
-    "System Speaker",
-    "System Timer",
-    "UMBus Root Bus Enumerator",
-    "Unknown Device",
-    "WAN Miniport*"
+	# "Remote Desktop Device Redirector Bus",
+	"SM Bus Controller",
+	"System CMOS/real time clock",
+	"System Speaker",
+	"System Timer",
+	"UMBus Root Bus Enumerator",
+	"Unknown Device",
+	"WAN Miniport*"
 )
 
 # No errors as some devices may not have an option to be disabled
--- src/Executables/STORAGESENSE.ps1
+++ src/Executables/STORAGESENSE.ps1
@@ -1,11 +1,11 @@
 # Reference: https://gist.github.com/he3als/3d9dcf6e796aa920c24a98130165fb17
 $properties = @{
 	# Enable Storage Sense
-    	"01" = 1
+ 	"01" = 1
 	# Run Storage Sense
 	"1024" = 1
 	# Run Storage Sense every month
-   	"2048" = 30
+	"2048" = 30
 	# Enable cleaning temp files
 	"04" = 1
 	# Disable downloads being cleared
@@ -21,7 +21,7 @@ $properties = @{
 $Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy"
 
 $properties.GetEnumerator() | ForEach-Object {
-    Set-ItemProperty -Path $Path -Name $_.Key -Type DWORD -Value $_.Value -Force
+	Set-ItemProperty -Path $Path -Name $_.Key -Type DWORD -Value $_.Value -Force
 }
 
 # Enable cleaning temp files
@@ -29,7 +29,7 @@ Enable-ScheduledTask -TaskName "Microsoft\Windows\DiskCleanup\SilentCleanup"
 
 # Disable OneDrive cleanup for subkeys
 Get-ChildItem -Path "$Path\OneDrive*" |
-    ForEach-Object {
-        Set-ItemProperty -Path $_.PSPath -Name "02" -Type DWORD -Value 0 -Force
+	ForEach-Object {
+		Set-ItemProperty -Path $_.PSPath -Name "02" -Type DWORD -Value 0 -Force
 		Set-ItemProperty -Path $_.PSPath -Name "128" -Type DWORD -Value 0 -Force
-    }
+	}
