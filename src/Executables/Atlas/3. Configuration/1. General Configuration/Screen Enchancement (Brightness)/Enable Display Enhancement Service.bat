47ae39cd066d61fd77379de7410d40bc6a0561d1
lemonek <72318911+lemonekq@users.noreply.github.com>
2023-06-08 22:17:27 +0200

    i need to learn about schtask...
    fixed

--- src/Executables/Atlas/3. Configuration/1. General Configuration/Screen Enchancement (Brightness)/Enable Display Enhancement Service.bat
+++ src/Executables/Atlas/3. Configuration/1. General Configuration/Screen Enchancement (Brightness)/Enable Display Enhancement Service.bat
@@ -1,4 +1,4 @@
 @echo off
 sc config DisplayEnhancementService start=auto
 net start DisplayEnhancementService
-schtasks /create /tn "Start Display Enhancement Service" /sc onstart /delay 0000:30 /rl highest /tr "net start DisplayEnhancementService" /f
+schtasks /create /tn "Start Display Enhancement Service" /sc onstart /delay 0000:30 /tr "net start DisplayEnhancementService" /f
