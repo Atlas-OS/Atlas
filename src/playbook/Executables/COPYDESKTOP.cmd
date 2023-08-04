5aa58874aca6e4fea47f19c7709a271c78071b24
he3als <65787561+he3als@users.noreply.github.com>
2023-08-04 23:03:59 +0100

    feat(COPYDESKTOP.cmd): prevent people deleting Atlas folder

--- src/playbook/Executables/COPYDESKTOP.cmd
+++ src/playbook/Executables/COPYDESKTOP.cmd
@@ -2,5 +2,11 @@
 setlocal EnableDelayedExpansion
 
 for /f "usebackq delims=" %%a in (`dir /b /a:d "!SystemDrive!\Users" ^| findstr /v /i /x /c:"Public" /c:"Default User" /c:"All Users"`) do (
-	robocopy "Atlas" "!SystemDrive!\Users\%%a\Desktop\Atlas" /E /PURGE /IM /IT /NP
+	set "atlasFolderPath=!SystemDrive!\Users\%%a\Desktop\Atlas"
+	robocopy "Atlas" "%atlasFolderPath%" /E /PURGE /IM /IT /NP
+	rem Prevent people from removing the Atlas folder
+	takeown /f "%atlasFolderPath%" /r /d y > nul
+	icacls "%atlasFolderPath%" /reset > nul
+	icacls "%atlasFolderPath%" /inheritance:r > nul
+	icacls "%atlasFolderPath%" /grant:r "SYSTEM:(OI)(CI)F" /grant:r "Users:(OI)(CI)RX" /grant:r "Administrators:(OI)(CI)RX" /t > nul
 )
