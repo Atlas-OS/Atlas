f2578f536f98798063d76a6648f4f135ff2a1582
Xyueta <vojtech@disroot.org>
2023-08-08 13:13:37 +0200

    feat: do not use idle disable when hyper threading is enabled

--- src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Power/CPU Idle/Disable Idle.cmd
+++ src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Power/CPU Idle/Disable Idle.cmd
@@ -1,15 +1,37 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
-	call RunAsTI.cmd "%~f0" "%*"
-	exit /b
+
+:: Check if hyper threading is enabled
+for /f "tokens=2 delims==" %%a in ('wmic cpu get NumberOfCores /value') do set PHYSICAL_CORES=%%a
+for /f "tokens=2 delims==" %%a in ('wmic cpu get NumberOfLogicalProcessors /value') do set LOGICAL_CORES=%%a
+
+if "%LOGICAL_CORES%" GTR "%PHYSICAL_CORES%" (
+	call :hyperThreading
 )
 
-echo THIS WILL CAUSE YOUR CPU USAGE TO *DISPLAY* AS 100% IN TASK MANAGER. ENABLE IDLE IF THIS IS AN ISSUE.
+echo This will make your Task Manager display CPU usage as 100% (visual bug). It does not occur in Process Explorer.
 powercfg /setacvalueindex scheme_current sub_processor 5d76a2ca-e8c0-402f-a133-2158492d58ad 1
 powercfg /setactive scheme_current
 
 echo Finished, changes have been applied.
 pause
 exit /b
+
+:hyperThreading
+mode con: cols=46 lines=14
+chcp 65001 > nul
+echo]
+echo [32m           Hyper Threading Detected
+echo   ──────────────────────────────────────────[0m
+echo   Detected Hyper Threading feature enabled.
+echo]
+echo   You [1mshould not disable idle states [0mwhen
+echo   this feature is enabled as it makes
+echo   the single-threaded performance worse.
+echo]
+echo   It can be disabled by using BIOS or GRUB.
+echo]
+echo            [1m[33mPress any key to exit...      [?25l
+pause > nul
+exit /b 1
