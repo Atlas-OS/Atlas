cc7b33c7fe0399b805e16c8013e2044afceaf465
Xyueta <vojtech@disroot.org>
2023-08-08 12:42:06 +0200

    feat: add global timer resolution requests @daBroma

--- src/playbook/Executables/WIN11.cmd
+++ src/playbook/Executables/WIN11.cmd
@@ -1,10 +1,14 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: Check for a Windows version and exit if W11 is not installed
+:: Check for a Windows build and exit if Windows 11 is not installed
 for /f "tokens=6 delims=[.] " %%a in ('ver') do (
     if %%a lss 22000 (
         exit /b
     )
 )
 
+:: Enable Global Timer Resolution requests
+:: https://github.com/amitxv/PC-Tuning/blob/main/docs/research.md#fixing-timing-precision-in-windows-after-the-great-rule-change
+:: https://randomascii.wordpress.com/2020/10/04/windows-timer-resolution-the-great-rule-change
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "GlobalTimerResolutionRequests" /t REG_DWORD /d "1" /f
