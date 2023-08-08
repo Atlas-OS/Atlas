9156b2804d675b457576d98006b3a88be1069593
Xyueta <vojtech@disroot.org>
2023-08-08 16:19:05 +0200

    feat: add an option to properly disable XBOX

--- src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Microsoft Store/Disable Microsoft Store.cmd
+++ src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Microsoft Store/Disable Microsoft Store.cmd
@@ -33,6 +33,15 @@ for %%a in (
 	call setSvc.cmd %%~a 4
 )
 
+if exist "C:\Program Files\WindowsApps\Microsoft.Xbox*" goto existXBOX
+goto end
+
+:existXBOX
+cls & choice /C YN /N /M "It appears XBOX-related applications are installed, would you like to remove them [Y/N]? "
+if !errorlevel! == 1 call "C:\Users\Default\Desktop\Atlas\3. Configuration\1. General Configuration\Xbox\Remove Xbox Applications.cmd" /silent
+if !errorlevel! == 2 goto end
+
+:end
 echo Finished, please reboot your device for changes to apply.
 pause
 exit /b
