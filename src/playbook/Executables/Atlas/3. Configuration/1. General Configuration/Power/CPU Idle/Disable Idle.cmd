851e804fd19209e679463735391d94dbc9d006ae
he3als <65787561+he3als@users.noreply.github.com>
2023-08-08 13:25:07 +0100

    fix(idle): general formatting & improved descriptions

--- src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Power/CPU Idle/Disable Idle.cmd
+++ src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Power/CPU Idle/Disable Idle.cmd
@@ -1,20 +1,21 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
-	call RunAsTI.cmd "%~f0" "%*"
-	exit /b
-)
-
 :: Check if hyper threading is enabled
 for /f "tokens=2 delims==" %%a in ('wmic cpu get NumberOfCores /value') do set PHYSICAL_CORES=%%a
 for /f "tokens=2 delims==" %%a in ('wmic cpu get NumberOfLogicalProcessors /value') do set LOGICAL_CORES=%%a
 
 if "%LOGICAL_CORES%" GTR "%PHYSICAL_CORES%" (
-	call :hyperThreading
+	goto :hyperThreading
 )
 
-echo This will make your Task Manager display CPU usage as 100% (visual bug). It does not occur in Process Explorer.
+echo This forces your CPU to work at its hardest always, ensure you have good cooling.
+echo]
+echo Task Manager will display CPU usage as 100%% always, due to how Task Manager calculates CPU percentage.
+echo It does not occur in Process Explorer, System Informer or Process Hacker.
+echo]
+pause
+cls
 powercfg /setacvalueindex scheme_current sub_processor 5d76a2ca-e8c0-402f-a133-2158492d58ad 1
 powercfg /setactive scheme_current
 
@@ -23,18 +24,19 @@ pause
 exit /b
 
 :hyperThreading
-mode con: cols=46 lines=14
+title Hyper Threading/SMT Detected
+mode con: cols=46 lines=13
 chcp 65001 > nul
 echo]
-echo [32m           Hyper Threading Detected
+echo [32m         Hyper Threading/SMT Detected
 echo   ──────────────────────────────────────────[0m
-echo   Detected Hyper Threading feature enabled.
-echo]
 echo   You [1mshould not disable idle states [0mwhen
-echo   this feature is enabled as it makes
-echo   the single-threaded performance worse.
+echo   this feature is enabled as it makes the
+echo   overall CPU performance much worse.
 echo]
-echo   It can be disabled by using BIOS or GRUB.
+echo   It can be disabled by using BIOS.
+echo   Instead of disabling idle, consider 
+echo   disabling C-states.
 echo]
 echo            [1m[33mPress any key to exit...      [?25l
 pause > nul
--- src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Power/CPU Idle/Enable Idle (default).cmd
+++ src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Power/CPU Idle/Enable Idle (default).cmd
@@ -1,11 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
-	call RunAsTI.cmd "%~f0" "%*"
-	exit /b
-)
-
 powercfg /setacvalueindex scheme_current sub_processor 5d76a2ca-e8c0-402f-a133-2158492d58ad 0
 powercfg /setactive scheme_current
 
