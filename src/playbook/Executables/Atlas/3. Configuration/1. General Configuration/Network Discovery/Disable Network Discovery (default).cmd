d7a5766cab99e5e6f6b18aafad8ba4cf3ee15989
Xyueta <vojtech@disroot.org>
2023-08-08 12:46:42 +0200

    feat: do not disable lanman workstation within network discovery

--- src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Network Discovery/Disable Network Discovery (default).cmd
+++ src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Network Discovery/Disable Network Discovery (default).cmd
@@ -6,15 +6,6 @@ whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 	exit /b
 )
 
-:: Disable Lanman Workstation (SMB)
-DISM /Online /Disable-Feature /FeatureName:"SmbDirect" /NoRestart
-call setSvc.cmd KSecPkg 4
-call setSvc.cmd LanmanWorkstation 4
-call setSvc.cmd mrxsmb 4
-call setSvc.cmd mrxsmb20 4
-call setSvc.cmd rdbss 3
-call setSvc.cmd srv2 4
-
 call setSvc.cmd fdPHost 4
 call setSvc.cmd FDResPub 4
 call setSvc.cmd lmhosts 4
