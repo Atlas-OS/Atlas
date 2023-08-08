81c397579170ccdb244cef058f9b216efdc908cc
Xyueta <vojtech@disroot.org>
2023-08-08 13:13:58 +0200

    fix: re add runasti snippet

--- src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Power/CPU Idle/Disable Idle.cmd
+++ src/playbook/Executables/Atlas/3. Configuration/1. General Configuration/Power/CPU Idle/Disable Idle.cmd
@@ -1,6 +1,10 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
+	call RunAsTI.cmd "%~f0" "%*"
+	exit /b
+)
 
 :: Check if hyper threading is enabled
 for /f "tokens=2 delims==" %%a in ('wmic cpu get NumberOfCores /value') do set PHYSICAL_CORES=%%a
