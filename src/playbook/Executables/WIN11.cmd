d194e37cac1254ad0b9a8f4f696f743a7aee0611
Xyueta <vojtech@disroot.org>
2023-08-09 13:18:56 +0200

    fix: calendar on windows 11

--- src/playbook/Executables/WIN11.cmd
+++ src/playbook/Executables/WIN11.cmd
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 
 :: Check for a Windows build and exit if Windows 11 is not installed
 for /f "tokens=6 delims=[.] " %%a in ('ver') do (
-    if %%a lss 22621 (
+    if %%a LSS 22621 (
         exit /b
     )
 )
@@ -30,3 +30,6 @@ reg add "HKCU\Software\Microsoft\input\Settings" /v "IsVoiceTypingKeyEnabled" /t
 
 :: Do not show files from Office.com
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "ShowCloudFilesInQuickAccess" /t REG_DWORD /d "0" /f
+
+:: Re-enable action center on Win11, as it breaks calendar
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /f
