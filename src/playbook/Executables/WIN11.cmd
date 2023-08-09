14a8dce97e86cbdf7d84f7ae4c71f33548701585
he3als <65787561+he3als@users.noreply.github.com>
2023-08-09 17:15:35 +0100

    fix(WIN11): apply to all users

--- src/playbook/Executables/WIN11.cmd
+++ src/playbook/Executables/WIN11.cmd
@@ -12,34 +12,40 @@ for /f "tokens=6 delims=[.] " %%a in ('ver') do (
     )
 )
 
-:: Set current user variable to apply HKCU
-set "current_user=%windir%\AtlasModules\Tools\NsudoLC.exe -U:E -P:E -Wait -ShowWindowMode:Hide"
-
 :: Enable Global Timer Resolution requests
 :: https://github.com/amitxv/PC-Tuning/blob/main/docs/research.md#fixing-timing-precision-in-windows-after-the-great-rule-change
 :: https://randomascii.wordpress.com/2020/10/04/windows-timer-resolution-the-great-rule-change
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "GlobalTimerResolutionRequests" /t REG_DWORD /d "1" /f
 
-:: Re-enable action center on Win11, as it breaks calendar
+for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
+	REM If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs do not have this key.
+	reg query "HKU\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_"
+	if not !errorlevel! == 1 (
+		call :registry "%%a"
+	)
+)
+
+:registry
+:: Re-enable Action Center on Win11, as it breaks calendar
 reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /f
 
 :: Do not show recommendations for tips, shortcuts, new apps, and more in start menu
-%current_user% reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_IrisRecommendations" /t REG_DWORD /d "0" /f
+reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_IrisRecommendations" /t REG_DWORD /d "0" /f
 
 :: Do not show account related notifications occasionally in start menu
-%current_user% reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_AccountNotifications" /t REG_DWORD /d "0" /f
+reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_AccountNotifications" /t REG_DWORD /d "0" /f
 
 :: Remove Widgets button from taskbar
-%current_user% reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarDa" /t REG_DWORD /d "0" /f
+reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarDa" /t REG_DWORD /d "0" /f
 
 :: Remove Chat button from taskbar
-%current_user% reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarMn" /t REG_DWORD /d "0" /f
+reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarMn" /t REG_DWORD /d "0" /f
 
 :: Do not show the voice typing microphone button
-%current_user% reg add "HKCU\Software\Microsoft\input\Settings" /v "IsVoiceTypingKeyEnabled" /t REG_DWORD /d "0" /f
+reg add "HKU\%~1\Software\Microsoft\input\Settings" /v "IsVoiceTypingKeyEnabled" /t REG_DWORD /d "0" /f
 
 :: Do not show files from Office.com
-%current_user% reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "ShowCloudFilesInQuickAccess" /t REG_DWORD /d "0" /f
+reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "ShowCloudFilesInQuickAccess" /t REG_DWORD /d "0" /f
 
 :: Restore old Windows 10 context menu
-%current_user% reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f
+reg add "HKU\%~1\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f
