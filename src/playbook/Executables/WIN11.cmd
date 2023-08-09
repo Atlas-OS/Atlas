c69180dff9e45e01aed66a26cbf6fd2a477b730a
Xyueta <vojtech@disroot.org>
2023-08-09 13:34:04 +0200

    feat: add new/old context menu toggles

--- src/playbook/Executables/WIN11.cmd
+++ src/playbook/Executables/WIN11.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: Check for a Windows build and exit if Windows 11 is not installed
+:: Check for a Windows build and make changes
 for /f "tokens=6 delims=[.] " %%a in ('ver') do (
     if %%a LSS 22621 (
         exit /b
@@ -35,5 +35,4 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "ShowCloudF
 reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /f
 
 :: Restore old Windows 10 context menu
-reg add "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /v "" /d "" /f
-
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve​
