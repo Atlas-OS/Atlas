6cb64e778fc3c02e7c2228fde92e6e475bf4e6ed
he3als <65787561+he3als@users.noreply.github.com>
2023-08-15 00:10:24 +0100

    feat(WINGET.ps1): don't install if already installed

--- src/playbook/Executables/WINGET.ps1
+++ src/playbook/Executables/WINGET.ps1
@@ -1,17 +1,20 @@
 # Credit: https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-on-windows-sandbox
 
 $progressPreference = 'silentlyContinue'
-Set-Location "C:\Windows\Temp"
 
-Write-Information "Downloading WinGet and its dependencies..."
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -UseBasicParsing
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-
-Write-Information "Installing WinGet and its dependencies..."
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+if ($null -eq $(cmd /c where winget)) {
+	Set-Location "C:\Windows\Temp"
+	
+	Write-Information "Downloading WinGet and its dependencies..."
+	Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -UseBasicParsing
+	Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+	Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+	
+	Write-Information "Installing WinGet and its dependencies..."
+	Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+	Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+	Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+}
 
 Write-Information "Configuring WinGet..."
 # This is only temporary to ensure reliability, it's reverted after all WinGet actions
