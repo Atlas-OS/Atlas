577e1d094092a16d3097f5e39d106e7af6a19eaa
he3als <65787561+he3als@users.noreply.github.com>
2023-08-17 23:43:25 +0100

    refactor: make everything else quiet

--- src/playbook/Executables/COPYDESKTOP.cmd
+++ src/playbook/Executables/COPYDESKTOP.cmd
@@ -2,5 +2,5 @@
 setlocal EnableDelayedExpansion
 
 for /f "usebackq delims=" %%a in (`dir /b /a:d "!SystemDrive!\Users" ^| findstr /v /i /x /c:"Public" /c:"Default User" /c:"All Users"`) do (
-	robocopy "Atlas" "!SystemDrive!\Users\%%a\Desktop\Atlas" /E /PURGE /IM /IT /NP
+	robocopy "Atlas" "!SystemDrive!\Users\%%a\Desktop\Atlas" /E /PURGE /IM /IT /NP > nul
 )
