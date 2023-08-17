cc95293a4a8f3b9b70417ff7b912bc5e12b14e4e
he3als <65787561+he3als@users.noreply.github.com>
2023-08-17 23:37:47 +0100

    refactor(THEME.cmd): make it quiet

--- src/playbook/Executables/THEME.cmd
+++ src/playbook/Executables/THEME.cmd
@@ -4,9 +4,9 @@
 copy "Themes\Atlas Dark.theme" "%windir%\Resources\Themes\aero.theme"
 for /f "tokens=6 delims=[.] " %%a in ('ver') do (
     if %%a LSS 22621 (
-		copy "Themes\Atlas Light.theme" "%windir%\Resources\Themes\Light.theme"
+		copy "Themes\Atlas Light.theme" "%windir%\Resources\Themes\Light.theme" > nul
     ) else (
-		copy "Themes\Atlas Light.theme" "%windir%\Resources\Themes\spotlight.theme"
+		copy "Themes\Atlas Light.theme" "%windir%\Resources\Themes\spotlight.theme" > nul
 		set win11=true
 	)
 )
@@ -24,24 +24,24 @@ for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /
 :: clear lockscreen cache
 for /d %%a in ("%ProgramData%\Microsoft\Windows\SystemData\*") do (
 	for /d %%c in ("%%a\ReadOnly\LockScreen_*") do (
-		rd /s /q "%%c"
+		rd /s /q "%%c" > nul
 	)
 )
 
-taskkill /F /IM sihost.exe
+taskkill /F /IM sihost.exe > nul
 exit /b
 
 :ALLUSERS
 if defined win11 (
-	reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Themes" /v "ThemeMRU" /t REG_SZ /d "C:\Windows\Resources\Themes\aero.theme;C:\Windows\resources\Themes\spotlight.theme;" /f
+	reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Themes" /v "ThemeMRU" /t REG_SZ /d "C:\Windows\Resources\Themes\aero.theme;C:\Windows\resources\Themes\spotlight.theme;" /f > nul
 )
 
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SystemProtectedUserData\%~1\AnyoneRead\Colors" /v "AccentColor" /t REG_DWORD /d "4290728257" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SystemProtectedUserData\%~1\AnyoneRead\Colors" /v "StartColor" /t REG_DWORD /d "4291969335" /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SystemProtectedUserData\%~1\AnyoneRead\Colors" /v "AccentColor" /t REG_DWORD /d "4290728257" /f > nul
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SystemProtectedUserData\%~1\AnyoneRead\Colors" /v "StartColor" /t REG_DWORD /d "4291969335" /f > nul
 
-del /q /f "%~2\Microsoft\Windows\Themes\TranscodedWallpaper"
-rmdir /q /s "%~2\Microsoft\Windows\Themes\CachedFiles"
+del /q /f "%~2\Microsoft\Windows\Themes\TranscodedWallpaper" > nul
+rmdir /q /s "%~2\Microsoft\Windows\Themes\CachedFiles" > nul
 
 if not "%~1"=="AME_UserHive_Default" (
-	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\%~1" /v "RotatingLockScreenEnabled" /t REG_DWORD /d "0" /f
+	reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\%~1" /v "RotatingLockScreenEnabled" /t REG_DWORD /d "0" /f > nul
 )
