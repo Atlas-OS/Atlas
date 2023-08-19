fa707c2d114ee77349ca00deec691b656cdb7b4b
he3als <65787561+he3als@users.noreply.github.com>
2023-08-19 14:06:58 +0100

    fix(DESKTOPSHORTCUTS): fix syntax

--- src/playbook/Executables/DESKTOPSHORTCUTS.ps1
+++ src/playbook/Executables/DESKTOPSHORTCUTS.ps1
@@ -5,6 +5,6 @@ $Shortcut = $WshShell.CreateShortcut($defaultShortcut)
 $Shortcut.TargetPath = "$env:WinDir\AtlasDesktop"
 $Shortcut.Save()
 
-foreach ($user in $(Get-ChildItem -Path "$env:SystemDrive\Users" -Directory | Where-Object { 'Public' -notcontains $_.Name }))) {
+foreach ($user in $(Get-ChildItem -Path "$env:SystemDrive\Users" -Directory | Where-Object { 'Public' -notcontains $_.Name })) {
     Copy-Item $defaultShortcut -Destination "$($user.FullName)\Desktop" -Force
 }
