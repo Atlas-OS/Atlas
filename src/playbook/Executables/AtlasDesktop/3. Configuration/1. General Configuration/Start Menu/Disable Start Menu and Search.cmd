b9b85d81cf3ce58061db7cb40bbd38139788aa6a
Xyueta <vojtech@disroot.org>
2023-08-20 21:22:36 +0200

    feat: simplify open-shell script

--- src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Start Menu/Disable Start Menu and Search.cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Start Menu/Disable Start Menu and Search.cmd
@@ -1,17 +1,23 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+if "%~1"=="/silent" goto main
+
 whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 	call RunAsTI.cmd "%~f0" "%*"
 	exit /b
 )
 
-if exist "C:\Program Files\Open-Shell" goto existS
-if exist "C:\Program Files (x86)\StartIsBack" goto existS
+if exist "!windir!\SystemApps\Microsoft.Windows.Search_cw5n1h2txyewy\SearchApp.exe" goto existS
+if exist "!windir!\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe" goto existS
+
+:existS
+if exist "C:\Program Files\Open-Shell" goto main
+if exist "C:\Program Files (x86)\StartIsBack" goto main
 echo It seems neither Open-Shell nor StartIsBack are not installed. It is HIGHLY recommended to install one of these before running this due to the Start Menu being removed.
 pause
 
-:existS
+:main
 set /P c="This will disable SearchApp and StartMenuExperienceHost, are you sure you want to continue [Y/N]? "
 if /I "!c!" == "Y" goto continSS
 if /I "!c!" == "N" exit /b
@@ -41,6 +47,8 @@ if exist "!windir!\SystemApps\Microsoft.Windows.Search_cw5n1h2txyewy\SearchApp.e
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d "0" /f > nul 2>&1
 taskkill /f /im explorer.exe > nul 2>&1
 start explorer.exe > nul 2>&1
+
+if "%~1"=="/silent" exit
 
 echo Finished, please reboot your device for changes to apply.
 pause
