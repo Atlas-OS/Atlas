9395124dd596d449786a862d647a89f5d0ee6577
he3als <65787561+he3als@users.noreply.github.com>
2023-08-21 13:32:47 +0100

    fix(THEME): copy themes properly

--- src/playbook/Executables/THEME.cmd
+++ src/playbook/Executables/THEME.cmd
@@ -1,15 +1,9 @@
 @echo off
+for /f "tokens=6 delims=[.] " %%a in ('ver') do (if %%a LSS 22000 set win11=true)
 
 :: copy themes
-copy "Themes\Atlas Dark.theme" "%windir%\Resources\Themes\aero.theme"
-for /f "tokens=6 delims=[.] " %%a in ('ver') do (
-    if %%a LSS 22621 (
-		copy /y "Themes\atlas-light.theme" "%windir%\Resources\Themes" > nul
-    ) else (
-		copy /y "Themes\atlas-dark.theme" "%windir%\Resources\Themes" > nul
-		set win11=true
-	)
-)
+copy /y "Themes\atlas-light.theme" "%windir%\Resources\Themes" > nul
+copy /y "Themes\atlas-dark.theme" "%windir%\Resources\Themes" > nul
 
 for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
 	if "%%a" == "AME_UserHive_Default" (
@@ -32,9 +26,7 @@ taskkill /F /IM sihost.exe > nul
 exit /b
 
 :ALLUSERS
-if defined win11 (
-	reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Themes" /v "ThemeMRU" /t REG_SZ /d "C:\Windows\Resources\Themes\atlas-dark.theme;C:\Windows\resources\Themes\atlas-light.theme;" /f > nul
-)
+if defined win11 reg add "HKU\%~1\Software\Microsoft\Windows\CurrentVersion\Themes" /v "ThemeMRU" /t REG_SZ /d "C:\Windows\Resources\Themes\atlas-dark.theme;C:\Windows\resources\Themes\atlas-light.theme;" /f > nul
 
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SystemProtectedUserData\%~1\AnyoneRead\Colors" /v "AccentColor" /t REG_DWORD /d "4290728257" /f > nul
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SystemProtectedUserData\%~1\AnyoneRead\Colors" /v "StartColor" /t REG_DWORD /d "4291969335" /f > nul
