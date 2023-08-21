39061b51b85f5ad4bd1736dc4cc1597095d16032
Xyueta <vojtech@disroot.org>
2023-08-21 23:45:53 +0200

    fix: powershell configuration

--- src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Network Discovery/Disable Network Discovery Services.cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Network Discovery/Disable Network Discovery Services.cmd
@@ -7,8 +7,8 @@ whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 )
 
 :: Unpin 'Network' to Explorer sidebar
-reg add "HKCU\Software\Classes\CLSID\{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f
-reg add "HKCU\Software\Classes\WOW6432Node\CLSID\{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f
+reg add "HKCR\CLSID\{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f
+reg add "HKCR\WOW6432Node\CLSID\{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d "0" /f
 
 call setSvc.cmd fdPHost 4
 call setSvc.cmd FDResPub 4
