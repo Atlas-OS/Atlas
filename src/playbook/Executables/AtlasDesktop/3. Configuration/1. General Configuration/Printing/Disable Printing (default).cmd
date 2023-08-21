d33227d7b85f15e92de06bf1c3d65f4dae75b2de
he3als <65787561+he3als@users.noreply.github.com>
2023-08-21 21:37:29 +0100

    fix(Enable Printing): suppress errors for context menu

--- src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Printing/Disable Printing (default).cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Printing/Disable Printing (default).cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-whoami /user | find /i "S-1-5-18"  > nul 2>&1 || (
+whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 	call RunAsTI.cmd "%~f0" "%*"
 	exit /b
 )
