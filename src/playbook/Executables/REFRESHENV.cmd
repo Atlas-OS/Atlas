bcff36ce973052e57f937ebfb372c5378859cdb0
Xyueta <vojtech@disroot.org>
2023-08-21 22:01:06 +0200

    fix: refreshenv.cmd (2)

--- src/playbook/Executables/REFRESHENV.cmd
+++ src/playbook/Executables/REFRESHENV.cmd
@@ -1,38 +1,40 @@
 @echo off
-:: A batch script to refresh environment variables from the registry
-:: Source: Chocolatey
-:: https://raw.githubusercontent.com/chocolatey-archive/chocolatey/master/src/redirects/RefreshEnv.cmd
+:: a batch script to refresh environment variables from the registry
+:: source: https://raw.githubusercontent.com/chocolatey-archive/chocolatey/master/src/redirects/RefreshEnv.cmd
+:: modified by Xyueta
+
+echo | set /p info="Refreshing environment variables. Please wait..."
 goto main
 
 :SetFromReg
-"%WinDir%\System32\Reg" QUERY "%~1" /v "%~2" > "%TEMP%\_envset.tmp" 2>NUL
-for /f "usebackq skip=2 tokens=2,*" %%A IN ("%TEMP%\_envset.tmp") do (
-    echo/set "%~3=%%B"
+reg query "%~1" /v "%~2" > "%temp%\_envset.tmp" 2>NUL
+for /f "usebackq skip=2 tokens=2,*" %%a in ("%temp%\_envset.tmp") do (
+    echo/set "%~3=%%b"
 )
-goto :EOF
+goto :eof
 
 :GetRegEnv
-"%WinDir%\System32\Reg" QUERY "%~1" > "%TEMP%\_envget.tmp"
-for /f "usebackq skip=2" %%A IN ("%TEMP%\_envget.tmp") do (
-    if /I not "%%~A"=="Path" (
-        call :SetFromReg "%~1" "%%~A" "%%~A"
+reg query "%~1" > "%temp%\_envget.tmp"
+for /f "usebackq skip=2" %%a in ("%temp%\_envget.tmp") do (
+    if /I not "%%~a"=="Path" (
+        call :SetFromReg "%~1" "%%~a" "%%~a"
     )
 )
-goto :EOF
+goto :eof
 
 :main
-echo/@echo off >"%TEMP%\_env.cmd"
-call :GetRegEnv "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" >> "%TEMP%\_env.cmd"
-call :GetRegEnv "HKCU\Environment">>"%TEMP%\_env.cmd" >> "%TEMP%\_env.cmd"
-call :SetFromReg "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" Path Path_HKLM >> "%TEMP%\_env.cmd"
-call :SetFromReg "HKCU\Environment" Path Path_HKCU >> "%TEMP%\_env.cmd"
-echo/set "Path=%%Path_HKLM%%;%%Path_HKCU%%" >> "%TEMP%\_env.cmd"
-del /f /q "%TEMP%\_envset.tmp" 2>nul
-del /f /q "%TEMP%\_envget.tmp" 2>nul
-SET "OriginalUserName=%USERNAME%"
-SET "OriginalArchitecture=%PROCESSOR_ARCHITECTURE%"
-call "%TEMP%\_env.cmd"
-del /f /q "%TEMP%\_env.cmd" 2>nul
-SET "USERNAME=%OriginalUserName%"
-SET "PROCESSOR_ARCHITECTURE=%OriginalArchitecture%"
+echo/@echo off >"%temp%\_env.cmd"
+call :GetRegEnv "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" >> "%temp%\_env.cmd"
+call :GetRegEnv "HKCU\Environment">>"%temp%\_env.cmd" >> "%temp%\_env.cmd"
+call :SetFromReg "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" Path Path_HKLM >> "%temp%\_env.cmd"
+call :SetFromReg "HKCU\Environment" Path Path_HKCU >> "%temp%\_env.cmd"
+echo/set "Path=%%Path_HKLM%%;%%Path_HKCU%%" >> "%temp%\_env.cmd"
+del /f /q "%temp%\_envset.tmp" 2>nul
+del /f /q "%temp%\_envget.tmp" 2>nul
+set "OriginalUserName=%USERNAME%"
+set "OriginalArchitecture=%PROCESSOR_ARCHITECTURE%"
+call "%temp%\_env.cmd"
+del /f /q "%temp%\_env.cmd" 2>nul
+set "USERNAME=%OriginalUserName%"
+set "PROCESSOR_ARCHITECTURE=%OriginalArchitecture%"
 exit /b
