e8e38705602ae4a64dae22038239e4764e9b613d
he3als <65787561+he3als@users.noreply.github.com>
2023-08-22 19:40:33 +0100

    feat: offline files support

--- src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Offline Files/Disable Offline Files (default).cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Offline Files/Disable Offline Files (default).cmd
@@ -1,17 +1,26 @@
 @echo off
-setlocal EnableDelayedExpansion
+::setlocal EnableDelayedExpansion
 
 whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 	call RunAsTI.cmd "%~f0" "%*"
 	exit /b
 )
 
+set "taskName=TaskName:      "
+
 net stop CSC > nul 2>&1
 net stop CscService > nul 2>&1
 call setSvc.cmd CSC 4
 call setSvc.cmd CscService 4
+
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\NetCache" /v "Enabled" /t REG_DWORD /d "0" /f > nul
-taskkill /f /im sihost.exe
+
+set "root=\Microsoft\Windows\Offline Files"
+schtasks /change /tn "%root%\Background Synchronization" /disable > nul 2>&1
+schtasks /change /tn "%root%\Logon Synchronization" /disable > nul 2>&1
+
+:: restart Explorer
+taskkill /f /im sihost.exe > nul 2>&1
 
 echo Completed.
 pause
--- src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Offline Files/Enable Offline Files.cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/1. General Configuration/Offline Files/Enable Offline Files.cmd
@@ -10,7 +10,14 @@ call setSvc.cmd CSC 1
 call setSvc.cmd CscService 2
 net start CSC > nul 2>&1
 net start CscService > nul 2>&1
+
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\NetCache" /v "Enabled" /t REG_DWORD /d "1" /f > nul
+
+set "root=\Microsoft\Windows\Offline Files"
+schtasks /change /tn "%root%\Background Synchronization" /disable > nul 2>&1
+schtasks /change /tn "%root%\Logon Synchronization" /disable > nul 2>&1
+
+:: restart explorer
 taskkill /f /im sihost.exe
 
 echo Completed.
