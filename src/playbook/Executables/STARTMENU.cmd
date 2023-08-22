d88ad2b24b4761027f38adecf467e45538f16ddc
Xyueta <vojtech@disroot.org>
2023-08-22 11:35:42 +0200

    fix: start menu configuration and get started removal

--- src/playbook/Executables/STARTMENU.cmd
+++ src/playbook/Executables/STARTMENU.cmd
@@ -1,24 +1,23 @@
 @echo off
 
-copy /y "Layout.xml" "%SystemDrive%\Users\Default\AppData\Local\Microsoft\Windows\Shell\StartMenuLayout.xml" > nul
-copy /y "Layout.json" "%SystemDrive%\Users\Default\AppData\Local\Microsoft\Windows\Shell\StartMenuLayout.json" > nul
+copy /y "Layout.xml" "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml" > nul
 
 :: If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs don't have this key.
 for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
 	reg query "HKEY_USERS\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul 2>&1
 	if not errorlevel 1 (
 		for /f "usebackq tokens=4* delims= " %%c in (`reg query "HKU\%%a\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Local AppData" ^| findstr /r /x /c:".*Local AppData[ ]*REG_SZ[ ].*"`) do (
-			copy /y "Layout.xml" "%%c\Microsoft\Windows\Shell\StartMenuLayout.xml" > nul
-			copy /y "Layout.json" "%%c\Microsoft\Windows\Shell\StartMenuLayout.json" > nul
+			copy /y "Layout.xml" "%%c\Microsoft\Windows\Shell\LayoutModification.xml" > nul
 
 			rem Clear Start Menu pinned items
 			for /f "usebackq delims=" %%d in (`dir /b "%%c\Packages" /a:d ^| findstr /c:"Microsoft.Windows.StartMenuExperienceHost"`) do (
-				for /f "usebackq delims=" %%e in (`dir /b "%%c\Packages\%%d\LocalState" /a:-d ^| findstr /R /c:"start.\.bin" /c:"start\.bin"`) do del /q /f "%%c\Packages\%%d\LocalState\%%e" > nul
+				for /f "usebackq delims=" %%e in (`dir /b "%%c\Packages\%%d\LocalState" /a:-d ^| findstr /R /c:"start.\.bin" /c:"start\.bin"`) do (
+					del /q /f "%%c\Packages\%%d\LocalState\%%e"
+				)
 			)
 		)
-		reg add "HKU\%%a\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "StartLayoutFile" /t REG_SZ /d "C:\Windows\StartMenuLayout.xml" /f
 		for /f "usebackq delims=" %%c in (`reg query "HKU\%%a\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount" ^| findstr /c:"start.tilegrid"`) do (
-			reg delete "%%c" /f > nul
+			reg delete "%%c" /f > nul 2>&1
 		)
 	)
 )
