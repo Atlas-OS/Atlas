1bd25fd68a76bab4941cf64bafc356611bd3e506
Xyueta <vojtech@disroot.org>
2023-09-01 14:47:32 +0200

    fix: automatically use .url with Internet Browser

--- src/playbook/Executables/FILEASSOC.cmd
+++ src/playbook/Executables/FILEASSOC.cmd
@@ -4,6 +4,6 @@ for /f "usebackq tokens=2 delims=\" %%A in (`reg query "HKEY_USERS" ^| findstr /
 	REM If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs don't have this key.
 	reg query "HKU\%%A" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > NUL 2>&1
 		if not errorlevel 1 (
-			PowerShell -NoP -ExecutionPolicy Bypass -File assoc.ps1 "Placeholder" "%%A" ".bmp:PhotoViewer.FileAssoc.Tiff" ".dib:PhotoViewer.FileAssoc.Tiff" ".jfif:PhotoViewer.FileAssoc.Tiff" ".jpe:PhotoViewer.FileAssoc.Tiff" ".jpeg:PhotoViewer.FileAssoc.Tiff" ".jpg:PhotoViewer.FileAssoc.Tiff" ".jxr:PhotoViewer.FileAssoc.Tiff" ".png:PhotoViewer.FileAssoc.Tiff" ".tif:PhotoViewer.FileAssoc.Tiff" ".tiff:PhotoViewer.FileAssoc.Tiff" ".wdp:PhotoViewer.FileAssoc.Tiff"
+			PowerShell -NoP -ExecutionPolicy Bypass -File assoc.ps1 "Placeholder" "%%A" ".bmp:PhotoViewer.FileAssoc.Tiff" ".dib:PhotoViewer.FileAssoc.Tiff" ".jfif:PhotoViewer.FileAssoc.Tiff" ".jpe:PhotoViewer.FileAssoc.Tiff" ".jpeg:PhotoViewer.FileAssoc.Tiff" ".jpg:PhotoViewer.FileAssoc.Tiff" ".jxr:PhotoViewer.FileAssoc.Tiff" ".png:PhotoViewer.FileAssoc.Tiff" ".tif:PhotoViewer.FileAssoc.Tiff" ".tiff:PhotoViewer.FileAssoc.Tiff" ".wdp:PhotoViewer.FileAssoc.Tiff" ".url:IE.AssocFile.URL" 
 	)
 )
