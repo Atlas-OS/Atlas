654a7486099712014774a64fa76946eeaec52ef1
he3als <65787561+he3als@users.noreply.github.com>
2023-09-04 20:26:11 +0100

    feat(atlasdesktop): icon

--- src/playbook/Executables/DESKTOPSHORTCUTS.ps1
+++ src/playbook/Executables/DESKTOPSHORTCUTS.ps1
@@ -3,6 +3,7 @@ $defaultShortcut = "$env:SystemDrive\Users\Default\Desktop\Atlas.lnk"
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($defaultShortcut)
 $Shortcut.TargetPath = "$env:WinDir\AtlasDesktop"
+$Shortcut.IconLocation = "$env:WinDir\AtlasModules\Other\atlas-folder.ico,0"
 $Shortcut.Save()
 
 foreach ($user in $(Get-ChildItem -Path "$env:SystemDrive\Users" -Directory | Where-Object { 'Public' -notcontains $_.Name })) {
