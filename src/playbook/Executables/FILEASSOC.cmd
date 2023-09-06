c6740c97f8340f56faa2b264435b55c48ffc4a0a
he3als <65787561+he3als@users.noreply.github.com>
2023-09-06 23:13:21 +0100

    feat: set browser associations

--- src/playbook/Executables/FILEASSOC.cmd
+++ src/playbook/Executables/FILEASSOC.cmd
@@ -1,9 +1,47 @@
 @echo off
 
-for /f "usebackq tokens=2 delims=\" %%A in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
-	REM If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs don't have this key.
-	reg query "HKU\%%A" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > NUL 2>&1
-		if not errorlevel 1 (
-			PowerShell -NoP -ExecutionPolicy Bypass -File assoc.ps1 "Placeholder" "%%A" ".bmp:PhotoViewer.FileAssoc.Tiff" ".dib:PhotoViewer.FileAssoc.Tiff" ".jfif:PhotoViewer.FileAssoc.Tiff" ".jpe:PhotoViewer.FileAssoc.Tiff" ".jpeg:PhotoViewer.FileAssoc.Tiff" ".jpg:PhotoViewer.FileAssoc.Tiff" ".jxr:PhotoViewer.FileAssoc.Tiff" ".png:PhotoViewer.FileAssoc.Tiff" ".tif:PhotoViewer.FileAssoc.Tiff" ".tiff:PhotoViewer.FileAssoc.Tiff" ".wdp:PhotoViewer.FileAssoc.Tiff" ".url:IE.AssocFile.URL" 
-	)
+set baseAssociations=".bmp:PhotoViewer.FileAssoc.Tiff"^
+ ".dib:PhotoViewer.FileAssoc.Tiff"^
+ ".jfif:PhotoViewer.FileAssoc.Tiff"^
+ ".jpe:PhotoViewer.FileAssoc.Tiff"^
+ ".jpeg:PhotoViewer.FileAssoc.Tiff"^
+ ".jpg:PhotoViewer.FileAssoc.Tiff"^
+ ".jxr:PhotoViewer.FileAssoc.Tiff"^
+ ".png:PhotoViewer.FileAssoc.Tiff"^
+ ".tif:PhotoViewer.FileAssoc.Tiff"^
+ ".tiff:PhotoViewer.FileAssoc.Tiff"^
+ ".wdp:PhotoViewer.FileAssoc.Tiff"^
+ ".url:IE.AssocFile.URL"
+
+set braveAssociations="Proto:https:BraveHTML"^
+ "Proto:http:BraveHTML"^
+ ".htm:BraveHTML"^
+ ".html:BraveHTML"^
+ ".pdf:BraveFile"^
+ ".shtml:BraveHTML"
+
+set libreWolfAssociations="Proto:https:LibreWolfHTM"^
+ "Proto:http:LibreWolfHTM"^
+ ".htm:LibreWolfHTM"^
+ ".html:LibreWolfHTM"^
+ ".pdf:LibreWolfHTM"^
+ ".shtml:LibreWolfHTM"
+
+set chromeAssociations="Proto:https:ChromeHTML"^
+ "Proto:http:ChromeHTML"^
+ ".htm:ChromeHTML"^
+ ".html:ChromeHTML"^
+ ".pdf:ChromeHTML"^
+ ".shtml:ChromeHTML"
+
+if "%~1"=="" set associations=%baseAssociations%
+if "%~1"=="Microsoft Edge" set associations=%baseAssociations%
+if "%~1"=="Brave" set associations=%baseAssociations% %braveAssociations%
+if "%~1"=="LibreWolf" set associations=%baseAssociations% %libreWolfAssociations%
+if "%~1"=="Google Chrome" set associations=%baseAssociations% %chromeAssociations%
+
+:: If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs don't have this key.
+for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
+	reg query "HKU\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul 2>&1
+	if not "%errorlevel%"=="1" powershell -NoP -EP Bypass -File assoc.ps1 "Placeholder" "%%a" %associations%
 )
