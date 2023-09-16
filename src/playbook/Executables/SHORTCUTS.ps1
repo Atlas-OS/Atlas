2edfaf2bbf87ff44bf5deca35a401cb8a49d23e1
he3als <65787561+he3als@users.noreply.github.com>
2023-09-16 20:15:08 +0100

    feat: services backup rewrite & no manual RunAsTI

---
+++ src/playbook/Executables/SHORTCUTS.ps1
@@ -0,0 +1,29 @@
+function New-Shortcut {
+    [CmdletBinding()]
+    param (
+        [Parameter( Mandatory = $True )][string]$ShortcutPath,
+        [Parameter( Mandatory = $True )][string]$Target,
+        [string]$Arguments,
+        [string]$Icon
+    )
+
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
+    $Shortcut.TargetPath = $Target
+    if ($null -ne $Icon) { $Shortcut.IconLocation = $Icon }
+    if ($null -ne $Arguments) { $Shortcut.Arguments = $Arguments }
+    $Shortcut.Save()
+}
+
+$defaultShortcut = "$env:SystemDrive\Users\Default\Desktop\Atlas.lnk"
+New-Shortcut -Icon "$env:WinDir\AtlasModules\Other\atlas-folder.ico,0" -Target "$env:WinDir\AtlasDesktop" -ShortcutPath $defaultShortcut
+foreach ($user in $(Get-ChildItem -Path "$env:SystemDrive\Users" -Directory | Where-Object { 'Public' -notcontains $_.Name })) {
+    Copy-Item $defaultShortcut -Destination "$($user.FullName)\Desktop" -Force
+}
+Copy-Item $defaultShortcut -Destination "$env:ProgramData\Microsoft\Windows\Start Menu\Programs" -Force
+
+$runAsTI = "$env:WinDir\AtlasModules\Scripts\RunAsTI.cmd"
+$default = "$env:WinDir\AtlasDesktop\8. Troubleshooting\Default"
+C:\Windows\AtlasModules\Scripts\RunAsTI.cmd C:\Windows\AtlasModules\Other\winServices.reg
+New-Shortcut -ShortcutPath "$default Windows Services and Drivers.lnk" -Target "$runAsTI" -Arguments "$env:WinDir\AtlasModules\Other\winServices.reg" -Icon "$env:WinDir\regedit.exe,1"
+New-Shortcut -ShortcutPath "$default Atlas Services and Drivers.lnk" -Target "$runAsTI" -Arguments "$env:WinDir\AtlasModules\Other\atlasServices.reg" -Icon "$env:WinDir\regedit.exe,1"
