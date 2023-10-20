02e4aecb39d3f4205dd50676fec5adcca27f1c80
Xyueta <vojtech@disroot.org>
2023-10-20 23:34:16 +0200

    refactor: move troubleshooting/diagnostics

--- src/playbook/Executables/AtlasDesktop/3. Configuration/Diagnostics & Troubleshooting/Disable Troubleshooting (default).cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/Diagnostics & Troubleshooting/Disable Troubleshooting (default).cmd
@@ -15,9 +15,6 @@ for %%a in (
 choice /c:yn /n /m "Would you like to disable Diagnostic Policy Service (DPS)? Note: It breaks Data Usage page in Settings [Y/N] "
 if %errorlevel% == 1 call setSvc.cmd DPS 4
 
-:: Disable DiagLog autologger
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\DiagLog" /v "Start" /t REG_DWORD /d "0" /f > nul
-
 echo Finished, please reboot your device for changes to apply.
 pause
 exit /b
