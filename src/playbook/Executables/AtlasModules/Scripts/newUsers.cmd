0d5cf630a7c4ac30bdc8f9ab2c728a49ff6f404b
Xyueta <vojtech@disroot.org>
2023-10-21 23:33:37 +0200

    fix: registry key

--- src/playbook/Executables/AtlasModules/Scripts/newUsers.cmd
+++ src/playbook/Executables/AtlasModules/Scripts/newUsers.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set title=Preparing Atlas user settings...
+set "title=Preparing Atlas user settings..."
 title %title%
 
 echo %title%
@@ -10,7 +10,7 @@ echo You'll be logged out, and once you login again, your new account will be re
 (
 taskkill /f /im explorer.exe
 call "%windir%\AtlasDesktop\3. Configuration\Visual Effects\Atlas Visual Effects (default).cmd" /silent
-reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search /v SearchboxTaskbarMode /t REG_DWORD /d 1 /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d "1" /f
 timeout /nobreak /t 10
 logoff
 ) > nul
