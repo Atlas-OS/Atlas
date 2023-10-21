bee791f7988b91f3adf1bf19e3b6a4b534ca66e4
Xyueta <vojtech@disroot.org>
2023-10-21 20:24:43 +0200

    fix: CBS script

--- src/playbook/Executables/CLIENTCBS.ps1
+++ src/playbook/Executables/CLIENTCBS.ps1
@@ -1,4 +1,7 @@
-## Remove ads from the 'Accounts' page in Immersive Control Panel
+# Remove ads from the 'Accounts' page in Immersive Control Panel
+# Made by Xyueta
+
+$Cbs = "$env:windir\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy"
 $content = Get-Content -Path "$Cbs\Public\wsxpacks.json"
 $pattern = '^\s*"Windows\.Settings\.Account".*'
 $modifiedContent = $content -replace $pattern, ''
