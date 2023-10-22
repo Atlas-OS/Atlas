c04491a91bb6afb71e28cd3aa6b5b7f565df3a98
he3als <65787561+he3als@users.noreply.github.com>
2023-10-22 11:55:32 +0100

    Revert "Merge branch 'new-ame' into dev"
    This reverts commit f7b7313018b666d0bd24d21da38388f2c95b8da4, reversing
    changes made to b1a0fd0766a101949927c162265ea457aa25aede.

--- src/playbook/Executables/AtlasDesktop/3. Configuration/Notifications/Disable Notifications.cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/Notifications/Disable Notifications.cmd
@@ -10,10 +10,13 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_NOTIFICATION_SOUND" /t REG_DWORD /d "0" /f > nul
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" /v "ToastEnabled" /t REG_DWORD /d "0" /f > nul
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" /v "NoCloudApplicationNotification" /t REG_DWORD /d "1" /f > nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /t REG_DWORD /d "1" /f > nul
 
 :: Re-enable action center on Windows 11 as it breaks calendar
 for /f "tokens=6 delims=[.] " %%a in ('ver') do (
-    if %%a LSS 22000 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /t REG_DWORD /d "1" /f > nul > nul
+    if %%a GEQ 22000 (
+        reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /f > nul 2>&1
+    )
 )
 
 echo Finished, please reboot your device for changes to apply.
