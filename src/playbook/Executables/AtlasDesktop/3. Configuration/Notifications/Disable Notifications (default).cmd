3cfef674a8a4a020674ec9f74ecf3745b49710a6
Xyueta <vojtech@disroot.org>
2023-10-29 22:56:30 +0100

    feat: disable notifications

--- src/playbook/Executables/AtlasDesktop/3. Configuration/Notifications/Disable Notifications (default).cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/Notifications/Disable Notifications (default).cmd
@@ -1,11 +1,14 @@
 @echo off
 
+if "%~1" == "/silent" goto main
+
 whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 	call RunAsTI.cmd "%~f0" %*
 	exit /b
 )
 
-call setSvc.cmd WpnService 4
+:main
+call %windir%\AtlasModules\Scripts\setSvc.cmd WpnService 4
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\userNotificationListener" /v "Value" /t REG_SZ /d "Deny" /f > nul
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_NOTIFICATION_SOUND" /t REG_DWORD /d "0" /f > nul
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" /v "ToastEnabled" /t REG_DWORD /d "0" /f > nul
@@ -15,6 +18,8 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotificatio
 for /f "tokens=6 delims=[.] " %%a in ('ver') do (
     if %%a LSS 22000 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /t REG_DWORD /d "1" /f > nul
 )
+
+if "%~1" == "/silent" exit
 
 echo Finished, please reboot your device for changes to apply.
 pause
