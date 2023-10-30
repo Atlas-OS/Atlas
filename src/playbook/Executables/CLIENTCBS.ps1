3723e3a966f430bccc20c1ad5d4bd84aebec8656
Xyueta <vojtech@disroot.org>
2023-10-30 14:41:30 +0100

    fix: deprovisioned apps & music/videos pin

---
+++ src/playbook/Executables/CLIENTCBS.ps1
@@ -0,0 +1,9 @@
+# Remove ads from the 'Accounts' page in Immersive Control Panel
+# Made by Xyueta
+
+$Cbs = "$env:windir\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy"
+$content = Get-Content -Path "$Cbs\Public\wsxpacks.json"
+$pattern = '^\s"Windows\.Settings\.Account".'
+$modifiedContent = $content -replace $pattern, ''
+$modifiedContent = ($modifiedContent | Where-Object { $_.Trim() -ne "" }) -join "`n"
+Set-Content -Path "$Cbs\Public\wsxpacks.json" -Value $modifiedContent
