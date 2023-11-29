116ffe8cfa32120f8ab477343e5689dca5bbe06f
he3als <65787561+he3als@users.noreply.github.com>
2023-11-29 22:50:13 +0000

    fix(open shell): notify user about winget check

--- src/playbook/Executables/AtlasDesktop/3. Configuration/Start Menu/Install Open-Shell.cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/Start Menu/Install Open-Shell.cmd
@@ -10,6 +10,7 @@ fltmc > nul 2>&1 || (
 )
 
 :: Check if WinGet is functional or not
+echo Checking if WinGet exists...
 call "%windir%\AtlasModules\Scripts\wingetCheck.cmd"
 if %ERRORLEVEL% NEQ 0 exit /b 1
 
