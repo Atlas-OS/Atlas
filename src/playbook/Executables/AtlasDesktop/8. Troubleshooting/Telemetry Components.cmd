036aa63b2b1e3740550f34c1f480a153a28bdfd9
he3als <65787561+he3als@users.noreply.github.com>
2024-01-04 20:36:09 +0000

    fix(telemetry components): clarity in docs

--- src/playbook/Executables/AtlasDesktop/8. Troubleshooting/Telemetry Components.cmd
+++ src/playbook/Executables/AtlasDesktop/8. Troubleshooting/Telemetry Components.cmd
@@ -25,20 +25,32 @@ if (!$?) {
 }
 if ($null -eq $packages) {$TelemetryEnabled = '(current)'} else {$TelemetryDisabled = '(current)'}
 
+
+function Write-BulletPoint($message) {
+	Write-Host " - " -ForegroundColor Green -NoNewline
+	Write-Host $message
+}
+
 function Menu {
 	Clear-Host
 	$ColourDisable = 'White'; $ColourEnable = $ColourDisable
 	if ($TelemetryDisabled) {$ColourDisable = 'Gray'} else {$ColourEnable = 'Gray'}
 
-	Write-Host @"
-This script will remove or add the package that removes telemetry components in Windows.
-Enabling telemetry will damage user privacy, but also is a more supported configuration of Windows.
+	Write-Host "This script adds or removes Atlas' NoTelemetry package, which eliminates telemetry components in Windows.`n" -ForegroundColor Cyan
 
-Your computer will auto-restart if you disable it.`n
-"@ -ForegroundColor Blue
+	Write-BulletPoint @"
+Removing the package restores the telemetry components, which can aid in troubleshooting. If
+   resolving the issue involves removing the package, please report this as an issue to Atlas.`n
+"@
+	Write-BulletPoint @"
+Atlas retains policies that should disable telemetry even after the package removal. These policies
+   do not work on Windows Home edition.
+"@
 
-	Write-Host "1) Disable telemetry components $TelemetryDisabled" -ForegroundColor $ColourDisable
-	Write-Host "2) Enable telemetry components $TelemetryEnabled`n" -ForegroundColor $ColourEnable
+	Write-Host "`n---------------------------------------------------------------------------------------------------------`n" -ForegroundColor Magenta
+
+	Write-Host "1) Add the Atlas-NoTelemetry package $TelemetryDisabled" -ForegroundColor $ColourDisable
+	Write-Host "2) Remove the Atlas-NoTelemetry package $TelemetryEnabled`n" -ForegroundColor $ColourEnable
 
 	Write-Host "Choose 1 or 2: " -NoNewline -ForegroundColor Yellow
 	$pageInput = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
@@ -47,11 +59,12 @@ Your computer will auto-restart if you disable it.`n
 		# Disable Defender
 		1 {
 			if ($TelemetryDisabled) {Menu}
+			Write-Host "After adding the package, your computer will automatically restart." -ForegroundColor Yellow; Pause
 			Clear-Host; Write-Host "Disabling telemetry... Your computer will auto-restart.`n" -ForegroundColor Green
 			& "$env:windir\AtlasModules\PackagesEnvironment\centralScript.ps1" -EverythingButDefender
 			if ($lastexitcode -ne 1) {
 				Write-Host "Something went wrong disabling telemetry." -ForegroundColor Red
-				Write-Host "See the documentation: https://docs.atlasos.net/troubleshooting-and-faq/failed-component-removal"
+				Write-Host "Report this to the GitHub issues or get support from the Atlas Discord or forums."
 				PauseNul; exit $exitcode
 			}
 			PauseNul
@@ -67,6 +80,7 @@ Your computer will auto-restart if you disable it.`n
 				} catch {
 					Write-Host "Something went wrong removing the package: $package" -ForegroundColor Red
 					Write-Host "$_`n" -ForegroundColor Red
+					Write-Host "Report this to the GitHub issues or get support from the Atlas Discord or forums."
 					PauseNul; exit 1
 				}
 			}
