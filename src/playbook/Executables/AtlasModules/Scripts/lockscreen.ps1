5c37012b78234fe4ba606e7efcce8f53e68cdadf
he3als <65787561+he3als@users.noreply.github.com>
2024-01-04 18:43:59 +0000

    fix(lockscreen): make it work

--- src/playbook/Executables/AtlasModules/Scripts/lockscreen.ps1
+++ src/playbook/Executables/AtlasModules/Scripts/lockscreen.ps1
@@ -1,9 +1,9 @@
 # Credit: https://superuser.com/a/1343640
 $imagePath = "$env:windir\AtlasModules\Wallpapers\lockscreen.png"
 
-$newImagePath = [System.IO.Path]::GetDirectoryName($imagePath) + '\' + (New-Guid).Guid + [System.IO.Path]::GetExtension($imagePath)
+$newImagePath = [System.IO.Path]::GetTempPath() + (New-Guid).Guid + [System.IO.Path]::GetExtension($imagePath)
 Copy-Item $imagePath $newImagePath
-[Windows.System.UserProfile.LockScreen,Windows.System.UserProfile,ContentType=WindowsRuntime] | Out-Null
+[Windows.System.UserProfile.LockScreen, Windows.System.UserProfile, ContentType = WindowsRuntime] | Out-Null
 Add-Type -AssemblyName System.Runtime.WindowsRuntime
 $asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
 Function Await($WinRtTask, $ResultType) {
@@ -17,7 +17,7 @@ Function AwaitAction($WinRtAction) {
     $netTask = $asTask.Invoke($null, @($WinRtAction))
     $netTask.Wait(-1) | Out-Null
 }
-[Windows.Storage.StorageFile,Windows.Storage,ContentType=WindowsRuntime] | Out-Null
+[Windows.Storage.StorageFile, Windows.Storage, ContentType = WindowsRuntime] | Out-Null
 $image = Await ([Windows.Storage.StorageFile]::GetFileFromPathAsync($newImagePath)) ([Windows.Storage.StorageFile])
 AwaitAction ([Windows.System.UserProfile.LockScreen]::SetImageFileAsync($image))
 Remove-Item $newImagePath
--- src/playbook/Executables/AtlasModules/Scripts/newUsers.ps1
+++ src/playbook/Executables/AtlasModules/Scripts/newUsers.ps1
@@ -3,7 +3,7 @@ $title = 'Preparing Atlas user settings...'
 $Host.UI.RawUI.WindowTitle = $title
 Write-Host $title -ForegroundColor Yellow
 Write-Host $('-' * ($title.length + 3)) -ForegroundColor Yellow
-Write-Host 'You''ll be logged out in 10 to 20 seconds, and once you login again, your new account will be ready for use.'
+Write-Host "You'll be logged out in 10 to 20 seconds, and once you login again, your new account will be ready for use."
 
 # Disable Windows 11 context menu & 'Gallery' in File Explorer
 if ([System.Environment]::OSVersion.Version.Build -ge 22000) {
@@ -11,11 +11,14 @@ if ([System.Environment]::OSVersion.Version.Build -ge 22000) {
     reg import "$env:windir\AtlasDesktop\4. Optional Tweaks\File Explorer Customization\Gallery\Disable Gallery (default).reg" *>$null
 }
 
+# Set lockscreen wallpaper
+& "$env:windir\AtlasModules\Scripts\lockscreen.ps1"
+
 # Disable 'Network' in navigation pane
 reg import "$env:windir\AtlasDesktop\3. Configuration\Network Discovery\Network Navigation Pane\Disable Network Navigation Pane (default).reg" *>$null
 
 # Set visual effects
-Start-Process -FilePath "$env:windir\AtlasDesktop\3. Configuration\Visual Effects\Atlas Visual Effects (default).cmd" -ArgumentList '/silent' -WindowStyle Hidden
+& "$env:windir\AtlasDesktop\3. Configuration\Visual Effects\Atlas Visual Effects (default).cmd" /silent
 
 # Pin 'Videos' and 'Music' folders to Home/Quick Acesss
 $o = new-object -com shell.application
