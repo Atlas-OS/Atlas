330ad5272ffbe8a6c59bc8301a3902773452e21b
he3als <65787561+he3als@users.noreply.github.com>
2024-01-16 22:27:20 +0000

    fix(backup): correct argument name

--- src/playbook/Executables/BACKUP.ps1
+++ src/playbook/Executables/BACKUP.ps1
@@ -1,9 +1,9 @@
 param (
 	[Parameter( Mandatory = $True )]
-	[string]$FileName
+	[string]$FilePath
 )
 
-if (Test-Path $FileName) { exit }
+if (Test-Path $FilePath) { exit }
 
 Add-Content -Path $FileName -Value "Windows Registry Editor Version 5.00"
 Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Services" | ForEach-Object {
@@ -11,8 +11,8 @@ Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Services" | ForEach-Object {
 		$startValue = (Get-ItemProperty -Path $_.PSPath -Name "Start").Start
 		for ($c = 0; $c -le 4; $c++) {
 			if ($startValue -eq "0x$c") {
-				Add-Content -Path $FileName -Value ("`n" + "[" + $_.Name + "]")
-				Add-Content -Path $FileName -Value ("`"Start`"=dword:0000000$c")
+				Add-Content -Path $FilePath -Value ("`n" + "[" + $_.Name + "]")
+				Add-Content -Path $FilePath -Value ("`"Start`"=dword:0000000$c")
 			}
 		}
 	}
