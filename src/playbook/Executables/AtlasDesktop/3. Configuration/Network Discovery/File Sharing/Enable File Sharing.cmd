acd5f6f14ea297e4db818d1edb583b25109e8fc6
he3als <65787561+he3als@users.noreply.github.com>
2024-03-01 22:48:07 +0000

    refactor: move file sharing to ps1 wrapper

--- src/playbook/Executables/AtlasDesktop/3. Configuration/Network Discovery/File Sharing/Enable File Sharing.cmd
+++ src/playbook/Executables/AtlasDesktop/3. Configuration/Network Discovery/File Sharing/Enable File Sharing.cmd
@@ -1,59 +1,19 @@
-<# : batch portion
 @echo off
-if "%*" == "" (
-    whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
-        call RunAsTI.cmd "%~f0" %*
-        exit /b
-    )
+set "script=%windir%\AtlasModules\Scripts\ScriptWrappers\EnableFileSharing.ps1"
+
+if not exist "%script%" (
+	echo Script not found.
+	echo "%script%"
+	exit /b 1
 )
 
-set args= & set "args1=%*"
-if defined args1 set "args=%args1:"='%"
-PowerShell -NoP "& ([Scriptblock]::Create((Get-Content '%~f0' -Raw))) %args%"
-exit /b %ERRORLEVEL%
-: end batch / begin PowerShell #>
+fltmc > nul 2>&1 || (
+	echo Administrator privileges are required.
+	PowerShell Start -Verb RunAs '%0' 2> nul || (
+		echo You must run this script as admin.
+		exit /b 1
+	)
+	exit /b
+)
 
-$networkDiscoveryConfigPath = "$env:windir\AtlasDesktop\3. Configuration\Network Discovery"
-
-# Enable network items
-Enable-NetAdapterBinding -Name "*" -ComponentID ms_msclient, ms_server, ms_lltdio, ms_rspndr | Out-Null
-
-# Enable Network Discovery services and its dependencies
-Start-Process -FilePath "$networkDiscoveryConfigPath\Enable Network Discovery Services (default)" -ArgumentList "/silent" -WindowStyle Hidden
-
-# Enable NetBios over TCP/IP
-$interfaces = Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces" -Recurse | Where-Object { $_.GetValue("NetbiosOptions") -ne $null }
-foreach ($interface in $interfaces) {
-    Set-ItemProperty -Path $interface.PSPath -Name "NetbiosOptions" -Value 2 | Out-Null
-}
-
-# Enable Net Bios service
-cmd /c "call setSvc.cmd NetBT 1"
-
-choice /c:yn /n /m "Would you like to change your network profile to 'Private'? [Y/N] "
-if ($LASTEXITCODE -eq 1) {
-    # Set network profile to 'Private Network'
-    $profiles = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles" -Recurse | Where-Object { $_.GetValue("Category") -ne $null }
-    foreach ($profile in $profiles) {
-        Set-ItemProperty -Path $profile.PSPath -Name "Category" -Value 1 | Out-Null
-    }
-
-    # Enable network discovery firewall rules
-    Get-NetFirewallRule | Where-Object { 
-        ($_.DisplayGroup -eq "File and Printer Sharing" -or $_.DisplayGroup -eq "Network Discovery") -and
-        $_.Profile -like "*Private*"
-    } | Enable-NetFirewallRule
-
-    # Set up network connected devices automatically
-    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\NcdAutoSetup\Private" -Force -EA SilentlyContinue | Out-Null
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\NcdAutoSetup\Private" -Name "AutoSetup" -Value 1 | Out-Null
-}
-
-choice /c:yn /n /m "Would you like to add the Network Navigation Pane to the Explorer sidebar? [Y/N] "
-if ($LASTEXITCODE -eq 1) {reg import "$networkDiscoveryConfigPath\Network Navigation Pane\User Network Navigation Pane choice.reg" | Out-Null}
-
-Clear-Host
-Write-Host "Completed!" -ForegroundColor Green
-Write-Host "Press any key to exit... " -NoNewLine
-$Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') | Out-Null
-exit
+powershell -EP Bypass -NoP Unblock-File -Path """$env:script""" -EA 0; ^& """$env:script""" %*
