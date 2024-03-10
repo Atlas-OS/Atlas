c14fdf1e8b11a07af034a3a95200e85a92c54cfd
he3als <65787561+he3als@users.noreply.github.com>
2024-03-10 20:03:47 +0000

    feat(SOFTWARE): output when installing/downloading

--- src/playbook/Executables/SOFTWARE.ps1
+++ src/playbook/Executables/SOFTWARE.ps1
@@ -18,19 +18,20 @@ Push-Location $tempDir
 
 # Brave
 if ($Brave) {
-	Write-Host "Installing Brave..."
+	Write-Output "Downloading Brave..."
 	& curl.exe -LSs "https://laptop-updates.brave.com/latest/winx64" -o "$tempDir\BraveSetup.exe"
 	if (!$?) {
 		Write-Error "Downloading Brave failed."
 		exit 1
 	}
 
+	Write-Output "Installing Brave..."
 	& "$tempDir\BraveSetup.exe" /silent /install 2>&1 | Out-Null
 
 	do {
 		$processesFound = Get-Process | Where-Object { "BraveSetup" -contains $_.Name } | Select-Object -ExpandProperty Name
 		if ($processesFound) {
-			Write-Host "Still running BraveSetup."
+			Write-Output "Still running BraveSetup."
 			Start-Sleep -Seconds 2
 		} else {
 			Remove-Item "$tempDir" -ErrorAction SilentlyContinue -Force -Recurse
@@ -43,23 +44,25 @@ if ($Brave) {
 
 # Firefox
 if ($Firefox) {
-	Write-Host "Installing Firefox..."
+	Write-Output "Downloading Firefox..."
 	& curl.exe -LSs "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US" -o "$tempDir\firefox.exe"
+	Write-Output "Installing Firefox..."
 	Start-Process -FilePath "$tempDir\firefox.exe" -WindowStyle Hidden -ArgumentList '/S /ALLUSERS=1' -Wait 2>&1 | Out-Null
 	exit
 }
 
 # Chrome
 if ($Chrome) {
-	Write-Host "Installing Google Chrome..."
+	Write-Output "Downloading Google Chrome..."
 	& curl.exe -LSs "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi" -o "$tempDir\chrome.msi"
+	Write-Output "Installing Google Chrome..."
 	Start-Process -FilePath "$tempDir\chrome.msi" -WindowStyle Hidden -ArgumentList '/qn' -Wait 2>&1 | Out-Null
 	exit
 }
 
-####################
-##    Software    ##
-####################
+#####################
+##    Utilities    ##
+#####################
 
 # Visual C++ Runtimes (referred to as vcredists for short)
 # https://learn.microsoft.com/en-US/cpp/windows/latest-supported-vc-redist
@@ -90,7 +93,7 @@ $vcredists = @{
 $num = 0; foreach ($a in $vcredists.GetEnumerator()) {
 	$num++; $vcredist = "$tempDir\vcredist$num.exe"
 	# curl is faster than Invoke-WebRequest
-	Write-Host "Installing Visual C++ Runtime $num..."
+	Write-Output "Downloading and installing Visual C++ Runtime $num..."
 	& curl.exe -LSs "$($a.Name)" -o "$vcredist"
 	Start-Process -FilePath $vcredist -WindowStyle Hidden -ArgumentList $a.Value -Wait 2>&1 | Out-Null
 }
@@ -98,14 +101,16 @@ $num = 0; foreach ($a in $vcredists.GetEnumerator()) {
 # 7-Zip
 $website = 'https://7-zip.org/'
 $download = $website + ((Invoke-WebRequest $website -UseBasicParsing).Links.href | Where-Object { $_ -like "a/7z*-$armString.exe" })
+Write-Output "Downloading 7-Zip..."
 & curl.exe -LSs $download -o "$tempDir\7zip.exe"
+Write-Output "Installing 7-Zip..."
 Start-Process -FilePath "$tempDir\7zip.exe" -WindowStyle Hidden -ArgumentList '/S' -Wait 2>&1 | Out-Null
 
 # Legacy DirectX runtimes
 & curl.exe -LSs "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe" -o "$tempDir\directx.exe"
-Write-Host "Extracting legacy DirectX runtimes..."
+Write-Output "Extracting legacy DirectX runtimes..."
 Start-Process -FilePath "$tempDir\directx.exe" -WindowStyle Hidden -ArgumentList "/q /c /t:`"$tempDir\directx`"" -Wait 2>&1 | Out-Null
-Write-Host "Installing legacy DirectX runtimes..."
+Write-Output "Installing legacy DirectX runtimes..."
 Start-Process -FilePath "$tempDir\directx\dxsetup.exe" -WindowStyle Hidden -ArgumentList '/silent' -Wait 2>&1 | Out-Null
 
 # Remove temporary directory
