456e033e290d8e10774b8a83ed252c1b5600c97a
he3als <65787561+he3als@users.noreply.github.com>
2024-03-18 18:59:32 +0000

    fix(admin snippet): pause if UAC declined

--- src/playbook/Executables/AtlasDesktop/8. Troubleshooting/Telemetry Components.cmd
+++ src/playbook/Executables/AtlasDesktop/8. Troubleshooting/Telemetry Components.cmd
@@ -12,6 +12,7 @@ fltmc > nul 2>&1 || (
 	echo Administrator privileges are required.
 	powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
 		echo You must run this script as admin.
+		if "%*"=="" pause
 		exit /b 1
 	)
 	exit /b
