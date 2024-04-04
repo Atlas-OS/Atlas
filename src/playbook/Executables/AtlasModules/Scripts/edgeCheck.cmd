8bf5751142874baa5909b907a99eb273a369bcbd
he3als <65787561+he3als@users.noreply.github.com>
2024-04-04 17:33:40 +0100

    fix(edgeCheck): better UX for just webview

--- src/playbook/Executables/AtlasModules/Scripts/edgeCheck.cmd
+++ src/playbook/Executables/AtlasModules/Scripts/edgeCheck.cmd
@@ -3,25 +3,22 @@
 set ___edge=0
 if exist "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" set ___edge=1
 if "%~1"=="/webview" set ___edge=1
-if %___edge%==1 (
-    set "text=WebView2 is"
-    set "plural=it"
-) else (
-    set "text=and Microsoft Edge WebView2 are"
-    set "plural=them"
-)
 
 set "___dashes=-----------------------------------------------------------------------------------------------------"
 echo %___dashes%
 
-echo Microsoft Edge %text% required to use this script.
-if %___edge%==0 echo In the future, if you no longer want to use this feature, you can use the disable script and uninstall Edge.
-choice /c:yn /n /m "Would you like to install %plural%? [Y/N] "
-if %errorlevel%==2 (
-    echo]
-    echo Press any key to exit...
-    pause > nul
-    exit /b
+if %___edge%==0 (
+    echo Microsoft Edge is required to use this script.
+    if %___edge%==0 echo In the future, if you no longer want to use this feature, you can use the disable script and uninstall Edge.
+    choice /c:yn /n /m "Would you like to install Edge? [Y/N] "
+    if %errorlevel%==2 (
+        echo]
+        echo Press any key to exit...
+        pause > nul
+        exit /b
+    )
+) else (
+    echo Updating Edge WebView 2...
 )
 
 echo]
@@ -33,7 +30,7 @@ if %___edge%==0 (
 )
 
 if "%errorlevel%"=="1" (
-    echo Something went wrong while trying to install Edge or WebView.
+    echo Something went wrong while trying to update/install Edge or WebView.
     if "%~1" neq "/silent" (
         echo Press any key to exit...
         pause > nul
