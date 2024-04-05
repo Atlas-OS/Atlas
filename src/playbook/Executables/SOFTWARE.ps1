74ae00c3bde4ac5cda42a15194500cecea1d85fc
he3als <65787561+he3als@users.noreply.github.com>
2024-04-05 12:02:40 +0100

    feat(SOFTWARE.ps1): noreboot on vcredists & more

--- src/playbook/Executables/SOFTWARE.ps1
+++ src/playbook/Executables/SOFTWARE.ps1
@@ -8,11 +8,12 @@ param (
 # Software is no longer installed with a package manager anymore to be as fast and as reliable as possible.   #
 # ----------------------------------------------------------------------------------------------------------- #
 
+$msiArgs = "/qn /quiet /norestart ALLUSERS=1 REBOOT=ReallySuppress"
 $arm = ((Get-CimInstance -Class Win32_ComputerSystem).SystemType -match 'ARM64') -or ($env:PROCESSOR_ARCHITECTURE -eq 'ARM64')
 $armString = ('x64', 'arm64')[$arm]
 
 # Create temporary directory
-$tempDir = Join-Path -Path $env:TEMP -ChildPath $([System.Guid]::NewGuid())
+$tempDir = Join-Path -Path $([System.IO.Path]::GetTempPath()) -ChildPath $([System.Guid]::NewGuid())
 New-Item $tempDir -ItemType Directory -Force | Out-Null
 Push-Location $tempDir
 
@@ -72,36 +73,54 @@ if ($Chrome) {
 
 # Visual C++ Runtimes (referred to as vcredists for short)
 # https://learn.microsoft.com/en-US/cpp/windows/latest-supported-vc-redist
-$legacyArgs1 = '/Q'
-$legacyArgs2 = '/q /norestart'
+$legacyArgs = '/q /norestart'
 $modernArgs = "/install /quiet /norestart"
 
-$vcredists = @{
+$vcredists = [ordered] @{
 	# 2005 - version 8.0.50727.6195 (MSI 8.0.61000/8.0.61001) SP1
-	"https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.exe" = $legacyArgs1
-	"https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.exe" = $legacyArgs1
+	"https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.exe" = @("2005-x64", "/c /q /t:")
+	"https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.exe" = @("2005-x86", "/c /q /t:")
 	# 2008 - version 9.0.30729.6161 (EXE 9.0.30729.5677) SP1
-	"https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x64.exe" = $legacyArgs1
-	"https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x86.exe" = $legacyArgs1
+	"https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x64.exe" = @("2008-x64", "/q /extract:")
+	"https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x86.exe" = @("2008-x86", "/q /extract:")
 	# 2010 - version 10.0.40219.325 SP1
-	"https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe" = $legacyArgs2
-	"https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe" = $legacyArgs2
+	"https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe" = @("2010-x64", $legacyArgs)
+	"https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe" = @("2010-x86", $legacyArgs)
 	# 2012 - version 11.0.61030.0
-	"https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe" = $modernArgs
-	"https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe" = $modernArgs
+	"https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe" = @("2012-x64", $modernArgs)
+	"https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe" = @("2012-x86", $modernArgs)
 	# 2013 - version 12.0.40664.0
-	"https://aka.ms/highdpimfc2013x64enu" = $modernArgs
-	"https://aka.ms/highdpimfc2013x86enu" = $modernArgs
+	"https://aka.ms/highdpimfc2013x64enu" = @("2013-x64", $modernArgs)
+	"https://aka.ms/highdpimfc2013x86enu" = @("2013-x86", $modernArgs)
 	# 2015-2022 (2015+) - latest version
-	"https://aka.ms/vs/17/release/vc_redist.x64.exe" = $modernArgs
-	"https://aka.ms/vs/17/release/vc_redist.x86.exe" = $modernArgs
+	"https://aka.ms/vs/17/release/vc_redist.x64.exe" = @("2015+-x64", $modernArgs)
+	"https://aka.ms/vs/17/release/vc_redist.x86.exe" = @("2015+-x86", $modernArgs)
 }
-$num = 0; foreach ($a in $vcredists.GetEnumerator()) {
-	$num++; $vcredist = "$tempDir\vcredist$num.exe"
+foreach ($a in $vcredists.GetEnumerator()) {
+	$vcName = $a.Value[0]
+	$vcArgs = $a.Value[1]
+	$vcUrl = $a.Name
+	$vcExePath = "$tempDir\vcredist-$vcName.exe"
+	
 	# curl is faster than Invoke-WebRequest
-	Write-Output "Downloading and installing Visual C++ Runtime $num..."
-	& curl.exe -LSs "$($a.Name)" -o "$vcredist"
-	Start-Process -FilePath $vcredist -WindowStyle Hidden -ArgumentList $a.Value -Wait 2>&1 | Out-Null
+	Write-Output "Downloading and installing Visual C++ Runtime $vcName..."
+	& curl.exe -LSs "$vcUrl" -o "$vcExePath"
+
+	if ($vcArgs -match ":") {
+		$msiDir = "$tempDir\vcredist-$vcName"
+		Start-Process -FilePath $vcExePath -ArgumentList "$vcArgs`"$msiDir`"" -Wait -WindowStyle Hidden
+		
+		$msiPaths = (Get-ChildItem -Path $msiDir -Filter *.msi -EA 0).FullName
+		if (!$msiPaths) {
+			Write-Output "Failed to extract MSI for $vcName, not installing."
+		} else {
+			$msiPaths | ForEach-Object {
+				Start-Process -FilePath "msiexec.exe" -ArgumentList "/log `"$msiDir\logfile.log`" /i `"$_`" $msiArgs" -WindowStyle Hidden
+			}
+		}
+	} else {
+		Start-Process -FilePath $vcExePath -ArgumentList $vcArgs -Wait -WindowStyle Hidden
+	}
 }
 
 # 7-Zip
