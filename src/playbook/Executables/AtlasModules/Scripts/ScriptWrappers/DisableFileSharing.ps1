f59dd2e60ad20a484dbe8f61f093eb44938f1c70
he3als <65787561+he3als@users.noreply.github.com>
2024-06-11 23:35:17 +0100

    fix(services scripts): new paths

--- src/playbook/Executables/AtlasModules/Scripts/ScriptWrappers/DisableFileSharing.ps1
+++ src/playbook/Executables/AtlasModules/Scripts/ScriptWrappers/DisableFileSharing.ps1
@@ -4,7 +4,7 @@ param (
     [switch]$Silent
 )
 
-$networkDiscoveryConfigPath = "$([Environment]::GetFolderPath('Windows'))\AtlasDesktop\3. General Configuration\Network Discovery"
+$fileSharingConfigPath = "$([Environment]::GetFolderPath('Windows'))\AtlasDesktop\3. General Configuration\File Sharing"
 
 # Disable network items
 Disable-NetAdapterBinding -Name "*" -ComponentID ms_msclient, ms_server, ms_lltdio, ms_rspndr | Out-Null
