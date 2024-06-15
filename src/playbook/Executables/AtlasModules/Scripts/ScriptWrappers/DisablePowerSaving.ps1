2a2380225a569a5b4c96dc4718ad09179677a3dc
he3als <65787561+he3als@users.noreply.github.com>
2024-06-15 17:50:43 +0100

    fix(power saving): full path to toggledev

--- src/playbook/Executables/AtlasModules/Scripts/ScriptWrappers/DisablePowerSaving.ps1
+++ src/playbook/Executables/AtlasModules/Scripts/ScriptWrappers/DisablePowerSaving.ps1
@@ -54,7 +54,7 @@ powercfg /setacvalueindex scheme_current 54533251-82be-4824-96c1-47b60b740d00 4d
 powercfg /setactive scheme_current
 
 Write-Host "Disabling power-saving ACPI devices..." -ForegroundColor Yellow
-& toggleDev.cmd -Disable '@("ACPI Processor Aggregator", "Microsoft Windows Management Interface for ACPI")' | Out-Null
+& "$([Environment]::GetFolderPath('Windows'))\AtlasModules\Scripts\toggleDev.cmd" -Disable '@("ACPI Processor Aggregator", "Microsoft Windows Management Interface for ACPI")' | Out-Null
 
 Write-Host "Disabling network adapter power-saving..." -ForegroundColor Yellow
 $properties = Get-NetAdapter -Physical | Get-NetAdapterAdvancedProperty
