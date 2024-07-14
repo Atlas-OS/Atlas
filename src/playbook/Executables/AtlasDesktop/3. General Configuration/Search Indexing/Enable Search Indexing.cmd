99c0ad3bc6b8c0015969a38e2d4f675765000708
nick <62725534+nicklvh@users.noreply.github.com>
2024-07-14 19:41:51 +0100

    feat(perf): respect power modes search indexing (#1147)

--- src/playbook/Executables/AtlasDesktop/3. General Configuration/Search Indexing/Enable Search Indexing.cmd
+++ src/playbook/Executables/AtlasDesktop/3. General Configuration/Search Indexing/Enable Search Indexing.cmd
@@ -20,6 +20,9 @@ fltmc > nul 2>&1 || (
 ) > nul
 call "%windir%\AtlasModules\Scripts\settingsPages.cmd" /unhide cortana-windowssearch
 
+:: Respect Power Settings when Search Indexing to prevent performance loss during gaming or other high-performance tasks
+reg add "HKLM\Software\Microsoft\Windows Search\Gather\Windows\SystemIndex" /v "RespectPowerModes" /t REG_DWORD /d "1" /f
+
 if "%~1"=="/silent" exit /b
 echo Finished, please reboot your device for changes to apply.
 pause
