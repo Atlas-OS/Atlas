b444adf998bbda3e4495ce819f624e55a13d78ba
he3als <65787561+he3als@users.noreply.github.com>
2024-08-08 16:43:45 +0100

    fix(local-build): if error, pause

--- src/playbook/local-build.cmd
+++ src/playbook/local-build.cmd
@@ -1,3 +1,6 @@
 @echo off
 echo Building Playbook...
 powershell -nop -ep bypass ^& "%cd%\..\local-build.ps1" -AddLiveLog -ReplaceOldPlaybook -Removals WinverRequirement, Verification -DontOpenPbLocation
+if %errorlevel% neq 0 (
+    if "%*"=="" pause
+)
--- src/playbook/local-build.sh
+++ src/playbook/local-build.sh
@@ -2,3 +2,8 @@
 
 echo "Building Playbook..."
 pwsh -NoP -EP Bypass -C "& \"$(dirname "$PWD")/local-build.ps1\" -AddLiveLog -ReplaceOldPlaybook -Removals WinverRequirement, Verification -DontOpenPbLocation"
+if [ $? -ne 0 ]; then
+    if [ -z "$*" ]; then
+        read -p "Press Enter to exit...: "
+    fi
+fi
