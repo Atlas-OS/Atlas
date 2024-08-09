d388df4f0091e68aba675035f5ee0fbee924b91b
he3als <65787561+he3als@users.noreply.github.com>
2024-08-09 01:32:25 +0100

    feat(rotating lockscreen): powershell rewrite

--- src/playbook/Executables/THEME.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
-	reg query "HKEY_USERS\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul && (
-		if not "%%a" == "AME_UserHive_Default" reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\%%a" /v "RotatingLockScreenEnabled" /t REG_DWORD /d "0" /f > nul
-	)
-)
-
-exit /b
