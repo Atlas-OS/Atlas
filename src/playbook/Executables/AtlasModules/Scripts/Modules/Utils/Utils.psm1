bd9214089adaa3fde44661d2ddcd23fb8ed0c74d
he3als <65787561+he3als@users.noreply.github.com>
2024-08-10 03:13:47 +0100

    feat(modules): Read-MessageBox for msg boxes

---
+++ src/playbook/Executables/AtlasModules/Scripts/Modules/Utils/Utils.psm1
@@ -0,0 +1,60 @@
+function Write-Title {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Text
+    )
+
+    $dashes = '-' * $text.Length
+    Write-Host "`n$dashes`n$text`n$dashes" -ForegroundColor Yellow
+}
+
+enum MsgIcon {
+    Stop
+    Question
+    Warning
+    Info
+}
+enum MsgButtons {
+    Ok
+    OkCancel
+    AbortRetryIgnore
+    YesNoCancel
+    YesNo
+    RetryAndCancel
+}
+function Read-MessageBox {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Title,
+        [Parameter(Mandatory=$true)]
+        [string]$Body,
+        [MsgIcon]$Icon = 'Info',
+        [MsgButtons]$Buttons = 'YesNo',
+        [int]$Timeout = 0,
+        [switch]$NoTopmost
+    )
+
+    $value = 0
+    $value += @(16, 32, 48, 64)[$([MsgIcon].GetEnumValues().IndexOf([MsgIcon]"$Icon"))] # icon
+    $value += [MsgButtons].GetEnumValues().IndexOf([MsgButtons]"$Buttons") # buttons
+    if (!$NoTopmost) { $value += 4096 } # topmost
+
+    $result = (New-Object -ComObject "Wscript.Shell").Popup($Body,$Timeout,$Title,$value)
+    $results = @{
+        1 = 'Ok'
+        2 = 'Cancel'
+        3 = 'Abort'
+        4 = 'Retry'
+        5 = 'Ignore'
+        6 = 'Yes'
+        7 = 'No'
+    }
+
+    if ($result) {
+        return $results.$result
+    } else {
+        return 'None'
+    }
+}
+
+Export-ModuleMember -Function Write-Title, Read-MessageBox
