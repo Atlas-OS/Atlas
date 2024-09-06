b664cf5e4bb6cbc73c07422a9f5908277b4567db
he3als <65787561+he3als@users.noreply.github.com>
2024-09-06 20:57:10 +0100

    feat(BACKUP): optimise it

--- src/playbook/Executables/BACKUP.ps1
+++ src/playbook/Executables/BACKUP.ps1
@@ -5,15 +5,18 @@ param (
 
 if (Test-Path $FilePath) { exit }
 
-Add-Content -Path $FilePath -Value "Windows Registry Editor Version 5.00"
+$content = [System.Collections.Generic.List[string]]::new()
+$content.Add("Windows Registry Editor Version 5.00")
 Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Services" | ForEach-Object {
 	if ((Get-ItemProperty $_.PSPath).PSObject.Properties.Name -contains "Start") {
 		$startValue = (Get-ItemProperty -Path $_.PSPath -Name "Start").Start
 		for ($c = 0; $c -le 4; $c++) {
 			if ($startValue -eq "0x$c") {
-				Add-Content -Path $FilePath -Value ("`n" + "[" + $_.Name + "]")
-				Add-Content -Path $FilePath -Value ("`"Start`"=dword:0000000$c")
+				$content.Add("`n[$($_.Name)]")
+				$content.Add('"Start"=dword:0000000' + $c)
 			}
 		}
 	}
 }
+
+Set-Content -Path $FilePath -Value $content -Force -Encoding UTF8
