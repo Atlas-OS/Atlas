d920bba83a3a1c91a23a3c0fcd2d6e12a29d916c
he3als <65787561+he3als@users.noreply.github.com>
2024-09-07 18:35:05 +0100

    fix(BACKUP): optimise further

--- src/playbook/Executables/BACKUP.ps1
+++ src/playbook/Executables/BACKUP.ps1
@@ -8,15 +8,11 @@ if (Test-Path $FilePath) { exit }
 $content = [System.Collections.Generic.List[string]]::new()
 $content.Add("Windows Registry Editor Version 5.00")
 Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Services" | ForEach-Object {
-	if ((Get-ItemProperty $_.PSPath).PSObject.Properties.Name -contains "Start") {
-		$startValue = (Get-ItemProperty -Path $_.PSPath -Name "Start").Start
-		for ($c = 0; $c -le 4; $c++) {
-			if ($startValue -eq "0x$c") {
-				$content.Add("`n[$($_.Name)]")
-				$content.Add('"Start"=dword:0000000' + $c)
-			}
-		}
-	}
+	try {
+		$startValue = Get-ItemPropertyValue -Path $_.PSPath -Name "Start" -EA Stop
+		$content.Add("`n[$($_.Name)]")
+		$content.Add('"Start"=dword:0000000' + $startValue)
+	} catch {}
 }
 
 Set-Content -Path $FilePath -Value $content -Force -Encoding UTF8
