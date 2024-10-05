3ac754ab1b4671da762c162e37fc0f427affce95
he3als <65787561+he3als@users.noreply.github.com>
2024-10-05 20:11:56 +0100

    feat(InstallSoftware): update links, add UniGetUI

--- src/playbook/Executables/AtlasModules/Scripts/ScriptWrappers/InstallSoftware.ps1
+++ src/playbook/Executables/AtlasModules/Scripts/ScriptWrappers/InstallSoftware.ps1
@@ -70,116 +70,119 @@ $Label.Text = "Download and install software using WinGet:"
 $Form.Controls.Add($Label)
 
 
-# https://winget.run/pkg/eloston/ungoogled-chromium
+# https://winstall.app/apps/eloston.ungoogled-chromium
 init_item "Ungoogled Chromium" "eloston.ungoogled-chromium"
 
-# https://winget.run/pkg/Mozilla/Firefox
+# https://winstall.app/apps/Mozilla.Firefox
 init_item "Mozilla Firefox" "Mozilla.Firefox"
 
-# https://winget.run/pkg/Waterfox/Waterfox
+# https://winstall.app/apps/Waterfox.Waterfox
 init_item "Waterfox" "Waterfox.Waterfox"
 
-# https://winget.run/pkg/Brave/brave
+# https://winstall.app/apps/Brave.brave
 init_item "Brave Browser" "Brave.Brave"
 
-# https://winget.run/pkg/Google/Chrome
+# https://winstall.app/apps/Google.Chrome
 init_item "Google Chrome" "Google.Chrome"
 
-# https://winget.run/pkg/LibreWolf/LibreWolf
+# https://winstall.app/apps/LibreWolf.LibreWolf
 init_item "LibreWolf" "LibreWolf.LibreWolf"
 
-# https://winget.run/pkg/TorProject/TorBrowser
+# https://winstall.app/apps/TorProject.TorBrowser
 init_item "Tor Browser" "TorProject.TorBrowser"
 
-# https://winget.run/pkg/Discord.Discord
+# https://winstall.app/apps/Discord.Discord
 init_item "Discord" "Discord.Discord"
 
-# https://winget.run/pkg/Discord/Discord.Canary
+# https://winstall.app/apps/Discord.Discord.Canary
 init_item "Discord Canary" "Discord.Discord.Canary"
 
-# https://winget.run/pkg/Valve/Steam
+# https://winstall.app/apps/Valve.Steam
 init_item "Steam" "Valve.Steam"
 
-# https://winget.run/pkg/Playnite/Playnite
+# https://winstall.app/apps/Playnite.Playnite
 init_item "Playnite" "Playnite.Playnite"
 
-# https://winget.run/pkg/HeroicGamesLauncher/HeroicGamesLauncher
+# https://winstall.app/apps/HeroicGamesLauncher.HeroicGamesLauncher
 init_item "Heroic" "HeroicGamesLauncher.HeroicGamesLauncher"
 
-# https://winget.run/pkg/voidtools/Everything
+# https://winstall.app/apps/voidtools.Everything
 init_item "Everything" "voidtools.Everything"
 
-# https://winget.run/pkg/Mozilla/Thunderbird
+# https://winstall.app/apps/Mozilla.Thunderbird
 init_item "Mozilla Thunderbird" "Mozilla.Thunderbird"
 
-# https://winget.run/pkg/PeterPawlowski/foobar2000
+# https://winstall.app/apps/PeterPawlowski.foobar2000
 init_item "foobar2000" "PeterPawlowski.foobar2000"
 
-# https://winget.run/pkg/IrfanSkiljan/IrfanView
+# https://winstall.app/apps/IrfanSkiljan.IrfanView
 init_item "IrfanView" "IrfanSkiljan.IrfanView"
 
-# https://winget.run/pkg/Git/Git
+# https://winstall.app/apps/Git.Git
 init_item "Git" "Git.Git"
 
-# https://winget.run/pkg/VideoLAN/VLC
+# https://winstall.app/apps/VideoLAN.VLC
 init_item "VLC" "VideoLAN.VLC"
 
-# https://winget.run/pkg/PuTTY/PuTTY
+# https://winstall.app/apps/PuTTY.PuTTY
 init_item "PuTTY" "PuTTY.PuTTY"
 
-# https://winget.run/pkg/Ditto/Ditto
+# https://winstall.app/apps/Ditto.Ditto
 init_item "Ditto" "Ditto.Ditto"
 
-# https://winget.run/pkg/7zip/7zip
+# https://winstall.app/apps/7zip.7zip
 init_item "7-Zip" "7zip.7zip"
 
-# https://winget.run/pkg/TeamSpeakSystems/TeamSpeakClient
+# https://winstall.app/apps/TeamSpeakSystems.TeamSpeakClient
 init_item "Teamspeak" "TeamSpeakSystems.TeamSpeakClient"
 
-# https://winget.run/pkg/Spotify/Spotify
+# https://winstall.app/apps/Spotify.Spotify
 init_item "Spotify" "Spotify.Spotify"
 
-# https://winget.run/pkg/OBSProject/OBSStudio
+# https://winstall.app/apps/OBSProject.OBSStudio
 init_item "OBS Studio" "OBSProject.OBSStudio"
 
-# https://winget.run/pkg/Guru3D/Afterburner
+# https://winstall.app/apps/Guru3D.Afterburner
 init_item "MSI Afterburner" "Guru3D.Afterburner"
 
-# https://winget.run/pkg/CPUID/CPU-Z
+# https://winstall.app/apps/CPUID.CPU-Z
 init_item "CPU-Z" "CPUID.CPU-Z"
 
-# https://winget.run/pkg/TechPowerUp/GPU-Z
+# https://winstall.app/apps/TechPowerUp.GPU-Z
 init_item "GPU-Z" "TechPowerUp.GPU-Z"
 
-# https://winget.run/pkg/Notepad++/Notepad++
+# https://winstall.app/apps/Notepad++.Notepad++
 init_item "Notepad++" "Notepad++.Notepad++"
 
-# https://winget.run/pkg/Microsoft/VisualStudioCode
+# https://winstall.app/apps/Microsoft.VisualStudioCode
 init_item "VSCode" "Microsoft.VisualStudioCode"
 
-# https://winget.run/pkg/VSCodium/VSCodium
+# https://winstall.app/apps/VSCodium.VSCodium
 init_item "VSCodium" "VSCodium.VSCodium"
 
-# https://winget.run/pkg/Klocman/BulkCrapUninstaller
+# https://winstall.app/apps/Klocman.BulkCrapUninstaller
 init_item "BCUninstaller" "Klocman.BulkCrapUninstaller"
 
-# https://winget.run/pkg/REALiX/HWiNFO
+# https://winstall.app/apps/REALiX.HWiNFO
 init_item "HWiNFO" "REALiX.HWiNFO"
 
-# https://winget.run/pkg/Skillbrains/Lightshot
+# https://winstall.app/apps/Skillbrains.Lightshot
 init_item "Lightshot" "Skillbrains.Lightshot"
 
-# https://winget.run/pkg/ShareX/ShareX
+# https://winstall.app/apps/ShareX.ShareX
 init_item "ShareX" "ShareX.ShareX"
 
 # https://www.microsoft.com/store/productId/9MZ95KL8MR0L?ocid=pdpshare
 init_item "Snipping Tool" "9MZ95KL8MR0L"
 
-# https://winget.run/pkg/valinet/ExplorerPatcher
+# https://winstall.app/apps/valinet.ExplorerPatcher
 init_item "ExplorerPatcher" "valinet.ExplorerPatcher"
 
-# https://winget.run/pkg/Microsoft/PowerShell
+# https://winstall.app/apps/Microsoft.PowerShell
 init_item "Powershell 7" "Microsoft.PowerShell"
+
+#https://winstall.app/apps/MartiCliment.UniGetUI
+init_item "UniGetUI" "MartiCliment.UniGetUI"
 
 if ([System.Environment]::OSVersion.Version.Build -ge 22000) {
     # https://winget.run/pkg/StartIsBack/StartAllBack
