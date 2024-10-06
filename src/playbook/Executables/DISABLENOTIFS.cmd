08331fa4c75c93322f455bf74d8a3bcde136a465
he3als <65787561+he3als@users.noreply.github.com>
2024-10-06 20:25:52 +0100

    fix: disable notifs for deployment

---
+++ src/playbook/Executables/DISABLENOTIFS.cmd
@@ -0,0 +1,35 @@
+@echo off
+
+:: Registry changes
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\userNotificationListener" /v "Value" /t REG_SZ /d "Deny" /f > nul
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_NOTIFICATION_SOUND" /t REG_DWORD /d "0" /f > nul
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" /v "ToastEnabled" /t REG_DWORD /d "0" /f > nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" /v "NoCloudApplicationNotification" /t REG_DWORD /d "1" /f > nul
+
+:: Hide Settings pages
+for %%a in (
+	"notifications"
+	"privacy-notifications"
+) do (
+	call "%windir%\AtlasModules\Scripts\settingsPages.cmd" /hide %%~a /silent
+)
+
+:: Disable notif center
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /t REG_DWORD /d "1" /f > nul
+
+:: Disable services
+sc config WpnService start=disabled > nul
+sc stop WpnService > nul 2>&1
+call "%windir%\AtlasModules\Scripts\setSvc.cmd" "WpnUserService" 4
+for /f "tokens=5 delims=\" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" ^| find "WpnUserService_"') do (
+	call "%windir%\AtlasModules\Scripts\setSvc.cmd" "%%a" 4
+	sc stop "%%a" > nul
+	sc delete "%%a" > nul
+)
+
+:: Apply changes
+taskkill /f /im explorer.exe > nul 2>&1
+taskkill /f /im ShellExperienceHost.exe > nul 2>&1
+start explorer.exe
+
+echo Disabled notifications.
---
+++ src/playbook/Executables/ENABLENOTIFS.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+:: Revert Registry changes
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" > nul
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\userNotificationListener" /v "Value" /t REG_SZ /d "Allow" /f > nul
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_NOTIFICATION_SOUND" /t REG_DWORD /d "1" /f > nul
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" /v "ToastEnabled" /t REG_DWORD /d "1" /f > nul
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" /v "NoCloudApplicationNotification" /f > nul 2>&1
+
+:: Unhide Settings pages
+for %%a in (
+	"notifications"
+	"privacy-notifications"
+) do (
+	call "%windir%\AtlasModules\Scripts\settingsPages.cmd" /unhide %%~a /silent
+)
+
+
+:: Apply changes
+taskkill /f /im explorer.exe > nul 2>&1
+taskkill /f /im ShellExperienceHost.exe > nul 2>&1
+start explorer.exe
+
+echo Enabled notifications.
