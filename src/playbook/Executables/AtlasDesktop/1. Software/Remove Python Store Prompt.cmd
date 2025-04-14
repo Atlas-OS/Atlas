9baceff55fd2dae15fff0d83fe6225ff7f89a78d
rad <80015713+RadNotRed@users.noreply.github.com>
2025-04-14 11:44:57 -0400

    fix: multiline parsing issue

--- src/playbook/Executables/AtlasDesktop/1. Software/Remove Python Store Prompt.cmd
+++ src/playbook/Executables/AtlasDesktop/1. Software/Remove Python Store Prompt.cmd
@@ -1,14 +1,7 @@
 @echo off
 
 echo Attempting to remove Python executables from WindowsApps...
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-"Remove-Item -Path \"$env:LOCALAPPDATA\Microsoft\WindowsApps\python*.exe\" -Force -ErrorAction SilentlyContinue; ^
-
-echo Checking and removing 'python' alias if it exists...
- if (Test-Path Alias:python) { Remove-Item Alias:python }; ^
-
-echo Checking and removing 'python3' alias if it exists...
- if (Test-Path Alias:python3) { Remove-Item Alias:python3 };
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Remove-Item -Path \"$env:LOCALAPPDATA\Microsoft\WindowsApps\python*.exe\" -Force -ErrorAction SilentlyContinue; if (Test-Path Alias:python) { Remove-Item Alias:python }; if (Test-Path Alias:python3) { Remove-Item Alias:python3 }"
 
 echo -----------------------------------------
 echo Cleanup completed.
