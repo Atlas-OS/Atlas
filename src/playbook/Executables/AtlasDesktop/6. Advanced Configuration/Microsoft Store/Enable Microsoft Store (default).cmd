f9ef9925faa72f698c67c6ea00c9acf7f436a136
RadNotRed <80015713+RadNotRed@users.noreply.github.com>
2025-06-23 13:54:44 -0400

    feat: add remove ms store and extra utils urls

---
+++ src/playbook/Executables/AtlasDesktop/6. Advanced Configuration/Microsoft Store/Enable Microsoft Store (default).cmd
@@ -0,0 +1,28 @@
+@echo off
+set "settingName=MicrosoftStore"
+set "stateValue=1"
+set "scriptPath=%~f0"
+set "___args="%~f0" %*"
+
+fltmc > nul 2>&1 || (
+    echo Administrator privileges are required.
+    powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList \"\"\"/c %^env:___args%\"\"\"" 2> nul || (
+        echo You must run this script as admin.
+        if "%*"=="" pause
+        exit /b 1
+    )
+    exit /b
+)
+
+reg add "HKLM\SOFTWARE\AtlasOS\%settingName%" /v state /t REG_DWORD /d %stateValue% /f > nul
+reg add "HKLM\SOFTWARE\AtlasOS\%settingName%" /v path  /t REG_SZ    /d "%scriptPath%" /f > nul
+
+powershell -NoP -NonI -Command "Get-AppxPackage -AllUsers Microsoft.WindowsStore | ForEach-Object {Add-AppxPackage -DisableDevelopmentMode -Register (Join-Path $_.InstallLocation 'AppXManifest.xml')}" > nul
+
+if "%~1"=="/silent" exit /b
+
+echo.
+echo Microsoft Store has been reinstalled/enabled.
+echo Press any key to exit...
+pause > nul
+exit /b 
