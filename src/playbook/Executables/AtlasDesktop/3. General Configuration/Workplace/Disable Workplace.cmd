206b3bf0806ea5217f9b4ba0b556c2bf2c05c4cf
rad <80015713+RadNotRed@users.noreply.github.com>
2025-08-01 12:13:31 -0400

    feat: enable and disable Workplace settings

---
+++ src/playbook/Executables/AtlasDesktop/3. General Configuration/Workplace/Disable Workplace.cmd
@@ -0,0 +1,29 @@
+@echo off
+set "settingName=Workplace"
+set "stateValue=0"
+set "scriptPath=%~f0"
+
+set "___args="%~f0" %*"
+fltmc > nul 2>&1 || (
+    echo Administrator privileges are required.
+    powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
+        echo You must run this script as admin.
+        if "%*"=="" pause
+        exit /b 1
+    )
+    exit /b
+)
+
+reg add "HKLM\SOFTWARE\AtlasOS\%settingName%" /v state /t REG_DWORD /d %stateValue% /f > nul
+reg add "HKLM\SOFTWARE\AtlasOS\%settingName%" /v path /t REG_SZ /d "%scriptPath%" /f > nul
+
+if not "%~1"=="/silent" call "%windir%\AtlasModules\Scripts\serviceWarning.cmd" %*
+
+call "%windir%\AtlasModules\Scripts\settingsPages.cmd" /hide workplace /silent
+
+if "%~1"=="/silent" exit /b
+
+echo.
+echo Workplace settings page has been hidden.
+pause
+exit /b
