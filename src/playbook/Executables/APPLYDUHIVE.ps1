cd4bb6dc74b8993b44c262581dc067a9924897cc
TheyCreeper <theycreeper@proton.me>
2025-09-23 10:34:30 -0400

    feat: updated applyduhive.ps1

--- src/playbook/Executables/APPLYDUHIVE.ps1
+++ src/playbook/Executables/APPLYDUHIVE.ps1
@@ -1,14 +1,11 @@
 # Load DefaultUser hive
-$module = Get-Module -Name "PSYaml"
+$module = Get-Module -Name "FXPSYaml"
 if (!$module) {
-    Install-Module "FXPSYaml"
+    Install-Module -Name FXPSYaml -Confirm:$false
 }
-$defaultUserHive = "C:\Users\Default\NTUSER.DAT"
-$mountPoint = "HKU\DefaultUserTemp"
-reg load $mountPoint $defaultUserHive
+$mountPoint = "Registry::HKU\.DEFAULT"
 
-# Parse YAML files in configuration folder to get HKCU registry paths
-$configurationFolder = Join-Path $PSScriptRoot "Configuration\tweaks"
+$configurationFolder = Join-Path $PSScriptRoot "..\Configuration\tweaks"
 $yamlFiles = Get-ChildItem -Path $configurationFolder -Filter *.yml -Recurse
 
 $RegistryPaths = @()
@@ -18,7 +15,7 @@ foreach ($yamlFile in $yamlFiles) {
     foreach ($entry in $parsedYaml) {
         foreach ($value in $entry.actions.path) {
             if ($value -like 'HKU\.DEFAULT*') {
-                if (!$RegistryPaths.Contains($value)) { $RegistryPaths += $value.Substring(5) }
+                if (!$RegistryPaths.Contains($value.Substring(13))) { $RegistryPaths += $value.Substring(13) }
             }
         }
     }
@@ -26,17 +23,24 @@ foreach ($yamlFile in $yamlFiles) {
 
 foreach ($path in $RegistryPaths) {
     $source = "$mountPoint\$path"
-    $destination = "HKCU\$path"
-    # Copy all values from source to destination
+    $destination = "Registry::HKCU\$path"
     $values = Get-ItemProperty -Path $source -ErrorAction SilentlyContinue
     if ($values) {
         foreach ($property in $values.PSObject.Properties) {
             if ($property.Name -ne "PSPath" -and $property.Name -ne "PSParentPath" -and $property.Name -ne "PSChildName" -and $property.Name -ne "PSDrive" -and $property.Name -ne "PSProvider") {
-                Set-ItemProperty -Path $destination -Name $property.Name -Value $property.Value
+                if (-not (Test-Path $destination)) {
+                    New-Item -Path $destination -Force | Out-Null
+                }
+                if (-not ((Get-ItemProperty $destination -ErrorAction SilentlyContinue).PSObject.Properties.Name -contains $property.Name)) {
+                    New-ItemProperty -Path $destination -Name $property.Name -Value $property.Value | Out-Null
+                }
+                else {
+                    Set-ItemProperty -Path $destination -Name $property.Value -Value $property.Value
+                }
             }
         }
     }
 }
-
+Remove-Module "FXPSYaml" -Force
 # Unload DefaultUser hive
 reg unload $mountPoint
