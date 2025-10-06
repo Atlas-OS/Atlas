66cf6a10cfb31faebb850fc1889e7fc576c685a2
Creeper <thecreepermdrl@gmail.com>
2025-10-06 11:21:20 -0400

    fix: added back newUser script for iso installs

--- src/playbook/Executables/AtlasModules/Scripts/newUsers.ps1
+++ src/playbook/Executables/AtlasModules/Scripts/newUsers.ps1
@@ -50,28 +50,5 @@ $Browser
 
 reg delete "HKLM\SOFTWARE\AtlasOS\TempKey" /f
 
-# Pin 'Videos' and 'Music' folders to Home/Quick Acesss
-$o = new-object -com shell.application
-$currentPins = $o.Namespace('shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}').Items() | ForEach-Object { $_.Path }
-foreach ($path in @(
-    [Environment]::GetFolderPath('MyVideos'),
-    [Environment]::GetFolderPath('MyMusic')
-)) {
-    if ($currentPins -notcontains $path) {
-        $o.Namespace($path).Self.InvokeVerb('pintohome')
-    }
-}
-
-# Set taskbar search box to an icon
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 1
-
-#Set all the tweaks from the playbook
-Invoke-AllDebloatOptimizations
-Invoke-AllMiscSystemUtilities
-Invoke-AllPerformanceOptimizations
-Invoke-AllPrivacyOptimizations
-Invoke-AllQolOptimizations
-Invoke-AllSystemScripts
-
 # Leave
 Start-Sleep 5 
