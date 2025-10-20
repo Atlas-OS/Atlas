6c34f0b3135387466cd19ac46939b90b923280f4
Jack Holmes <74987167+jacksyrm@users.noreply.github.com>
2025-10-20 14:57:02 +0100

    refactor: clean up formatting and improve readability in CLEANUP.ps1

--- src/playbook/Executables/CLEANUP.ps1
+++ src/playbook/Executables/CLEANUP.ps1
@@ -1,48 +1,49 @@
 .\AtlasModules\initPowerShell.ps1
 function Invoke-AtlasDiskCleanup {
-	# Kill running cleanmgr instances, as they will prevent new cleanmgr from starting
-	Get-Process -Name cleanmgr -EA 0 | Stop-Process -Force -EA 0
-	# Disk Cleanup preset
-	# 2 = enabled
-	# 0 = disabled
-	$baseKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches'
-	$regValues = @{
-		"Active Setup Temp Folders" = 2
-		"BranchCache" = 2
-		"D3D Shader Cache" = 0
-		"Delivery Optimization Files" = 2
-		"Diagnostic Data Viewer database files" = 2
-		"Downloaded Program Files" = 2
-		"Internet Cache Files" = 2
-		"Language Pack" = 0
-		"Old ChkDsk Files" = 2
-		"Recycle Bin" = 0
-		"RetailDemo Offline Content" = 2
-		"Setup Log Files" = 2
-		"System error memory dump files" = 2
-		"System error minidump files" = 2
-		"Temporary Files" = 0
-		"Thumbnail Cache" = 2
-		"Update Cleanup" = 0
-		"User file versions" = 2
-		"Windows Error Reporting Files" = 2
-		"Windows Defender" = 2
-		"Temporary Sync Files" = 2
-		"Device Driver Packages" = 2
-	}
-	foreach ($entry in $regValues.GetEnumerator()) {
-		$key = "$baseKey\$($entry.Key)"
+    # Kill running cleanmgr instances, as they will prevent new cleanmgr from starting
+    Get-Process -Name cleanmgr -EA 0 | Stop-Process -Force -EA 0
+    # Disk Cleanup preset
+    # 2 = enabled
+    # 0 = disabled
+    $baseKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches'
+    $regValues = @{
+        "Active Setup Temp Folders"             = 2
+        "BranchCache"                           = 2
+        "D3D Shader Cache"                      = 0
+        "Delivery Optimization Files"           = 2
+        "Diagnostic Data Viewer database files" = 2
+        "Downloaded Program Files"              = 2
+        "Internet Cache Files"                  = 2
+        "Language Pack"                         = 0
+        "Old ChkDsk Files"                      = 2
+        "Recycle Bin"                           = 0
+        "RetailDemo Offline Content"            = 2
+        "Setup Log Files"                       = 2
+        "System error memory dump files"        = 2
+        "System error minidump files"           = 2
+        "Temporary Files"                       = 0
+        "Thumbnail Cache"                       = 2
+        "Update Cleanup"                        = 0
+        "User file versions"                    = 2
+        "Windows Error Reporting Files"         = 2
+        "Windows Defender"                      = 2
+        "Temporary Sync Files"                  = 2
+        "Device Driver Packages"                = 2
+    }
+    foreach ($entry in $regValues.GetEnumerator()) {
+        $key = "$baseKey\$($entry.Key)"
 
-		if (!(Test-Path $key)) {
-			Write-Output "'$key' not found, not configuring it."
-		} else {
-			Set-ItemProperty -Path "$baseKey\$($entry.Key)" -Name 'StateFlags0064' -Value $entry.Value -Type DWORD
-		}
-	}
+        if (!(Test-Path $key)) {
+            Write-Output "'$key' not found, not configuring it."
+        }
+        else {
+            Set-ItemProperty -Path "$baseKey\$($entry.Key)" -Name 'StateFlags0064' -Value $entry.Value -Type DWORD
+        }
+    }
 
-	# Run preset 64 (0-65535)
-	# As cleanmgr has multiple processes, there's no point in making the window hidden as it won't apply
-	Start-Process -FilePath "cleanmgr.exe" -ArgumentList "/sagerun:64" 2>&1 | Out-Null
+    # Run preset 64 (0-65535)
+    # As cleanmgr has multiple processes, there's no point in making the window hidden as it won't apply
+    Start-Process -FilePath "cleanmgr.exe" -ArgumentList "/sagerun:64" 2>&1 | Out-Null
 }
 
 # Check for other installations of Windows
@@ -50,49 +51,51 @@ function Invoke-AtlasDiskCleanup {
 $noCleanmgr = $false
 $drives = (Get-PSDrive -PSProvider FileSystem).Root | Where-Object { $_ -notmatch $(Get-SystemDrive) }
 foreach ($drive in $drives) {
-	if (Test-Path -Path $(Join-Path -Path $drive -ChildPath 'Windows') -PathType Container) {
-		Write-Output "Not running Disk Cleanup, other Windows drives found."
-		$noCleanmgr = $true
-		break
-	}
+    if (Test-Path -Path $(Join-Path -Path $drive -ChildPath 'Windows') -PathType Container) {
+        Write-Output "Not running Disk Cleanup, other Windows drives found."
+        $noCleanmgr = $true
+        break
+    }
 }
 
 if (!$noCleanmgr) {
-	Write-Output "No other Windows drives found, running Disk Cleanup."
-	Invoke-AtlasDiskCleanup
+    Write-Output "No other Windows drives found, running Disk Cleanup."
+    Invoke-AtlasDiskCleanup
 }
 
 # Clear the user temp folder
 foreach ($path in @($env:temp, $env:tmp, "$env:localappdata\Temp")) {
-	if (Test-Path $path -PathType Container) {
-		$userTemp = $path
-		break
-	}
+    if (Test-Path $path -PathType Container) {
+        $userTemp = $path
+        break
+    }
 }
-if ($path) {
-	Write-Output "Cleaning user TEMP folder..."
-	Get-ChildItem -Path $userTemp | Where-Object { $_.Name -ne 'AME' } | Remove-Item -Force -Recurse -EA 0
-} else {
-	Write-Error "User temp folder not found!"
+if ($userTemp) {
+    Write-Output "Cleaning user TEMP folder..."
+    Get-ChildItem -Path $userTemp | Where-Object { $_.Name -ne 'AME' } | Remove-Item -Force -Recurse -EA 0
+}
+else {
+    Write-Error "User temp folder not found!"
 }
 
 # Clear the system temp folder
 $machine = [System.EnvironmentVariableTarget]::Machine
 foreach ($path in @(
-	[System.Environment]::GetEnvironmentVariable("Temp", $machine),
-	[System.Environment]::GetEnvironmentVariable("Tmp", $machine),
-	"$([Environment]::GetFolderPath('Windows'))\Temp"
-)) {
-	if (Test-Path $path -PathType Container) {
-		$sysTemp = $path
-		break
-	}
+        [System.Environment]::GetEnvironmentVariable("Temp", $machine),
+        [System.Environment]::GetEnvironmentVariable("Tmp", $machine),
+        "$([Environment]::GetFolderPath('Windows'))\Temp"
+    )) {
+    if (Test-Path $path -PathType Container) {
+        $sysTemp = $path
+        break
+    }
 }
 if ($sysTemp) {
-	Write-Output "Cleaning system TEMP folder..."
-	Remove-Item -Path "$sysTemp\*" -Force -Recurse -EA 0
-} else {
-	Write-Error "System temp folder not found!"
+    Write-Output "Cleaning system TEMP folder..."
+    Remove-Item -Path "$sysTemp\*" -Force -Recurse -EA 0
+}
+else {
+    Write-Error "System temp folder not found!"
 }
 
 # Delete all system restore points
