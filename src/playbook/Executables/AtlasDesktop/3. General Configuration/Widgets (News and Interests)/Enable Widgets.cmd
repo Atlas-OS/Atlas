733792348326bf3688dc00b0b91c7568430f752b
frbee <193054852+frbee@users.noreply.github.com>
2025-10-23 18:22:36 +0800

    fix: fix for ShowCopilotButton in disable-copilot fix: fix encoding for '-' feat: update appx

--- src/playbook/Executables/AtlasDesktop/3. General Configuration/Widgets (News and Interests)/Enable Widgets.cmd
+++ src/playbook/Executables/AtlasDesktop/3. General Configuration/Widgets (News and Interests)/Enable Widgets.cmd
@@ -26,7 +26,7 @@ echo Enabling News and Interests (called Widgets in Windows 11)...
 (
     reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" /v "EnableFeeds" /f
     reg delete "HKLM\SOFTWARE\Policies\Microsoft\Dsh" /v "AllowNewsAndInterests" /f
-    powershell -command "stop-process -name explorer –force"
+    powershell -command "stop-process -name explorer -force"
 ) > nul 2>&1
 
 timeout /t 3 /nobreak > nul
